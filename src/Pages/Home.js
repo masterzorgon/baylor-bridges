79ee7a6f02345ee333d46a52f0d0413d9b3369b9
Cloudy Young <cloudy.young@outlook.com>
2022-01-10 01:46:18 -0700

    Update map color

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,10 +7,85 @@ class Home extends React.Component {
         // TODO: Display right panel for alumini list
     }
 
+    getAllStates() {
+        return [
+            "AZ",
+            "NY",
+            "CT",
+            "MD",
+            "WA",
+            "OR",
+            "NV",
+            "NM",
+            "DC",
+            "DE",
+            "MA",
+            "MN",
+            "WI",
+            "IL",
+            "VT",
+            "RI",
+            "NJ",
+            "CO",
+            "CA",
+            "PA",
+            "VA",
+            "GA",
+            "ME",
+            "NH",
+            "HI",
+            "ID",
+            "MT",
+            "IN",
+            "TE",
+            "AK",
+            "KY",
+            "NC",
+            "WV",
+            "WY",
+            "ND",
+            "SD",
+            "NE",
+            "UT",
+            "TN",
+            "KS",
+            "OK",
+            "TX",
+            "IO",
+            "MO",
+            "AR",
+            "AL",
+            "MS",
+            "LA",
+            "MI",
+            "LA",
+            "FL",
+            "SC",
+            "OH",
+            "IA",
+        ];
+    }
+
+    makeStatsConfig() {
+        const config = {};
+        const states = this.getAllStates();
+        
+        states.forEach((state) => {
+            var opacity = Math.random();
+
+            config[state] = {};
+            config[state].fill = `rgba(21, 71, 52, ${opacity})`; // Cloudy: This is Baylor green!
+        });
+
+        return config;
+    }
+
     render() {
+        const statesCustomConfig = this.makeStatsConfig();
+
         return (
             <>
-                <USAMap onClick={this.mapHandler} />
+                <USAMap onClick={this.mapHandler} customize={statesCustomConfig} />
             </>
         );
     }
