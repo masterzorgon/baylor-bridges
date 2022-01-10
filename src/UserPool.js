e3cd187a5c175152a93ea0cdd2d1eaaf4141a40f
JosephYu1 <Joseph_Yu1@baylor.edu>
2022-01-10 00:26:58 -0600

    configured poolData in UserPool.js

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,8 +1,8 @@
 import {CognitoUserPool} from "amazon-cognito-identity-js"
 
 const poolData={
-    UserPoolId:"us-east-2_phN1rHU8I",
-    ClientId:"5oventbh8mh2noja8sc4la2q1g"
+    UserPoolId:"us-east-2_O1ku8d52G",
+    ClientId:"7o86gkq6a707l4qm79ef1sq4h8"
 }
 
 export default new CognitoUserPool(poolData);
