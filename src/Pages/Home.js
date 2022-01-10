cff83a6a799f4590b2537ef5675d8bf2aa4ebd80
Cloudy Young <cloudy.young@outlook.com>
2022-01-10 01:22:16 -0700

    Import react-usa-map lib

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import USAMap from "react-usa-map";
 
-function Home(){
-    return(
-        <div>
-            this is home
-        </div>
-    )
+class Home extends React.Component {
+    mapHandler(event){
+        alert(event.target.dataset.name);
+        // TODO: Display right panel for alumini list
+    }
+
+    render() {
+        return (
+            <>
+                <USAMap onClick={this.mapHandler} />
+            </>
+        );
+    }
 }
 
 export default Home;
