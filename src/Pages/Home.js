15e28e8fb01274284370b05caf0a5984709ba71c
Cloudy Young <cloudy.young@outlook.com>
2022-01-10 18:56:07 -0700

    Create dummy commit for fixing merge

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,6 +6,7 @@ class Home extends React.Component {
         alert(event.target.dataset.name);
         // TODO: Display right panel for alumini list
     }
+    
 
     getAllStates() {
         return [
