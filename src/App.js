1259e5e7ad2cc629a72b25c3bba9a4a41ca713ca
Cloudy Young <cloudy.young@outlook.com>
2022-01-10 18:56:38 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Header from "./components/header";
+import Header from "./components/Header";
 import Home from "./Pages/Home"
 import About from "./Pages/About";
 import Testing from "./Pages/testPage";
