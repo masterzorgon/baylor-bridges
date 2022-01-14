58ecc072328fde837cc807a99f8035476229ed4c
Cloudy Young <cloudy.young@outlook.com>
2022-01-14 03:41:39 -0700

    Add more dummy data

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -71,6 +71,114 @@ const applications = [
     },
     {
         applicant: {
+            name: "Ted Fox",
+            email: "ted.fox@example.com",
+            imageUrl:
+                "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+        date: "2020-01-07",
+        dateFull: "January 7, 2020",
+        stage: "Completed phone screening",
+        href: "#",
+    },
+    {
+        applicant: {
+            name: "Ted Fox",
+            email: "ted.fox@example.com",
+            imageUrl:
+                "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+        date: "2020-01-07",
+        dateFull: "January 7, 2020",
+        stage: "Completed phone screening",
+        href: "#",
+    },
+    {
+        applicant: {
+            name: "Ted Fox",
+            email: "ted.fox@example.com",
+            imageUrl:
+                "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+        date: "2020-01-07",
+        dateFull: "January 7, 2020",
+        stage: "Completed phone screening",
+        href: "#",
+    },
+    {
+        applicant: {
+            name: "Ted Fox",
+            email: "ted.fox@example.com",
+            imageUrl:
+                "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+        date: "2020-01-07",
+        dateFull: "January 7, 2020",
+        stage: "Completed phone screening",
+        href: "#",
+    },
+    {
+        applicant: {
+            name: "Ted Fox",
+            email: "ted.fox@example.com",
+            imageUrl:
+                "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+        date: "2020-01-07",
+        dateFull: "January 7, 2020",
+        stage: "Completed phone screening",
+        href: "#",
+    },
+    {
+        applicant: {
+            name: "Ted Fox",
+            email: "ted.fox@example.com",
+            imageUrl:
+                "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+        date: "2020-01-07",
+        dateFull: "January 7, 2020",
+        stage: "Completed phone screening",
+        href: "#",
+    },
+    {
+        applicant: {
+            name: "Ted Fox",
+            email: "ted.fox@example.com",
+            imageUrl:
+                "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+        date: "2020-01-07",
+        dateFull: "January 7, 2020",
+        stage: "Completed phone screening",
+        href: "#",
+    },
+    {
+        applicant: {
+            name: "Ted Fox",
+            email: "ted.fox@example.com",
+            imageUrl:
+                "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+        date: "2020-01-07",
+        dateFull: "January 7, 2020",
+        stage: "Completed phone screening",
+        href: "#",
+    },
+    {
+        applicant: {
+            name: "Ted Fox",
+            email: "ted.fox@example.com",
+            imageUrl:
+                "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        },
+        date: "2020-01-07",
+        dateFull: "January 7, 2020",
+        stage: "Completed phone screening",
+        href: "#",
+    },
+    {
+        applicant: {
             name: "Ricardo Cooper",
             email: "ricardo.cooper@example.com",
             imageUrl:
