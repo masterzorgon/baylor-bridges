a003fcc4e01c7cce27abcf8632e39c5c56013973
Cloudy Young <cloudy.young@outlook.com>
2022-01-15 00:20:06 -0700

    Update hero section display on mobile
    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -53,7 +53,7 @@ class Home extends React.Component {
                 <div>
                     {/* Hero card */}
                     {/* FIXME: Hero card seems not fitting */}
-                    <div className="relative my-6">
+                    <div className="relative sm:my-6">
                         <div className="max-w-7xl mx-auto sm:px-6 lg:px-6">
                             <div className="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
                                 <div className="absolute inset-0">
