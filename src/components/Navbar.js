3fce285db91c2fd2eb32702d1b3345cea97c86c7
Cloudy Young <cloudy.young@outlook.com>
2022-01-15 00:14:19 -0700

    Merge AccountStatus component in Account

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 /* This example requires Tailwind CSS v2.0+ */
-import React, { Fragment, useState } from "react";
-import { Popover, Transition } from "@headlessui/react";
-import { MenuIcon, XIcon, SearchIcon } from "@heroicons/react/outline";
+import React, { Fragment, useState, useEffect, useContext } from "react";
+import { Popover, Transition, Menu } from "@headlessui/react";
+import { MenuIcon, XIcon, SearchIcon, BellIcon } from "@heroicons/react/outline";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 
-import NavbarAccount from "./NavbarAccount";
+import { AccountContext } from "./Account";
 
 
 function classNames(...classes) {
@@ -35,6 +35,16 @@ const people = [
 const Navbar = (props) => {
     const [isFocus, setFocus] = useState(false);
     const [searchText, setSearchText] = useState("");
+
+    // For current signed in account display
+    const [isSignedIn, setSignedIn] = useState(false);
+    const { getSession, logout } = useContext(AccountContext);
+
+    useEffect(() => {
+        getSession().then(session => {
+            setSignedIn(true);
+        });
+    });
 
     return (
         <>
@@ -174,7 +184,86 @@ const Navbar = (props) => {
                         </div>
                         
                         {/* Account sign in / up / out */}
-                        <NavbarAccount />
+                        {
+                            !isSignedIn &&
+                            <div className="flex items-center md:ml-12">
+                                <a href="/sign-in" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                                    Sign in
+                                </a>
+                                <a href="/sign-up" className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-emerald-600 hover:bg-emerald-700">
+                                    Sign up
+                                </a>
+                            </div>
+                        }
+                        {
+                            isSignedIn &&
+                            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 mr-4 gap-3">
+                                <button
+                                    type="button"
+                                    className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
+                                >
+                                    <span className="sr-only">View notifications</span>
+                                    <BellIcon className="h-6 w-6" aria-hidden="true" />
+                                </button>
+
+                                {/* Profile dropdown */}
+                                <Menu as="div" className="ml-3 relative">
+                                    <div>
+                                        <Menu.Button className="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500">
+                                            <span className="sr-only">Open user menu</span>
+                                            <img
+                                                className="h-8 w-8 rounded-full"
+                                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                                alt=""
+                                            />
+                                        </Menu.Button>
+                                    </div>
+                                    <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-200"
+                                        enterFrom="transform opacity-0 scale-95"
+                                        enterTo="transform opacity-100 scale-100"
+                                        leave="transition ease-in duration-75"
+                                        leaveFrom="transform opacity-100 scale-100"
+                                        leaveTo="transform opacity-0 scale-95"
+                                    >
+                                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <a
+                                                        href="/"
+                                                        className={classNames(active ? "bg-gray-100" : "", "block px-4 py-2 text-sm text-gray-700")}
+                                                    >
+                                                        Your Profile
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <a
+                                                        href="/"
+                                                        className={classNames(active ? "bg-gray-100" : "", "block px-4 py-2 text-sm text-gray-700")}
+                                                    >
+                                                        Settings
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <a
+                                                        href="/"
+                                                        className={classNames(active ? "bg-gray-100" : "", "block px-4 py-2 text-sm text-gray-700")}
+                                                        onClick={logout}
+                                                    >
+                                                        Sign out
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                        </Menu.Items>
+                                    </Transition>
+                                </Menu>
+                            </div>
+                        }
                     </div>
                 </div>
 
