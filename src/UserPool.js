247b4032b305b0e073fe50e590a680ae92b6a044
Cloudy Young <cloudy.young@outlook.com>
2022-01-16 17:51:12 -0700

    Remove cognito

--- src/UserPool.js
+++
@@ -1,8 +0,0 @@
-import {CognitoUserPool} from "amazon-cognito-identity-js";
-
-const poolData={
-    UserPoolId:"us-east-2_O1ku8d52G",
-    ClientId:"7o86gkq6a707l4qm79ef1sq4h8"
-};
-
-export default new CognitoUserPool(poolData);
--- src/components/Account.js
+++ src/components/Account.js
@@ -1,6 +1,4 @@
 import React, {createContext} from "react";
-import {CognitoUser, AuthenticationDetails} from "amazon-cognito-identity-js";
-import Pool from "../UserPool";
 import axios from "axios";
 
 const AccountContext = createContext();
@@ -10,61 +8,19 @@ const Account = (props) => {
     // TODO since authentication is on backend, fix this function
     const getSession = async () => {
         return await new Promise((resolve, reject) => {
-            const user = Pool.getCurrentUser();
-            if (user) {
-                user.getSession((err, session) => {
-                    if (err) {
-                        reject();
-                    } else {
-                        resolve(session);
-                    }
-                });
-            } else {
-                reject();
-            }
+            
         });
     };
 
     const authenticate = async (Username, Password) => {
         return await new Promise((resolve, reject) => {
-            const user = new CognitoUser({ Username, Pool });
-    
-            const authDetails = new AuthenticationDetails({ Username, Password });
-    
-            user.authenticateUser(authDetails,{
-                onSuccess: (data) => {
-                    console.log("Log In Success!", data);
-                    resolve(data);
-                },
-                onFailure: (err) => {
-                    console.error("Log In Error!", err);
-                    reject(err);
-                    // TODO: add failure message to UI for user
-                    
-                },
-                newPasswordRequired: (data) => {
-                    console.log("New Password Required!", data);
-                    resolve(data);
-                }
-            });
+            
         });
     };
 
     const logout = () => {
-        // const user = Pool.getCurrentUser();
-        // if (user) {
-        //     user.signOut();
-        //     window.location.href="/";
-        // }
         axios.post("/logOut").then(response=>{
-            const res=response.data;
-            console.log(res);
-            if(res.status==="success"){
-                // window.location.href="/";
-                console.log("success");
-            }else{
-                console.error("error message",res.message);
-            }
+            
         }).catch(err=>{
             console.error("catch error: ",err);
         });
