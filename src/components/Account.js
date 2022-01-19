c7d0ced905aef8f2cf72382cc42c8bcbdb8647c2
Cloudy Young <cloudy.young@outlook.com>
2022-01-18 23:04:09 -0700

    Update Account.js

--- src/components/Account.js
+++ src/components/Account.js
@@ -36,6 +36,7 @@ const Account = (props) => {
                     reject(response.data);
                 }
             }).catch(error => {
+                window.localStorage.removeItem("account");
                 reject(error);
             });
         });
