939e7a9eb071280abb8d4ba11bb7397817fff594
Tina <xiayan_li2@baylor.edu>
2022-01-20 16:23:08 +0800

    changing landing page background

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -61,7 +61,7 @@ const Home = () => {
                             <div className="absolute inset-0">
                                 <img
                                     className="h-full w-full object-cover"
-                                    src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
+                                    src="landing_page_background.jfif"
                                     alt="People working on laptops"
                                 />
                                 <div className="absolute inset-0 bg-emerald-700 mix-blend-multiply" />
@@ -122,7 +122,7 @@ const Home = () => {
                             <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
                                 <img 
                                     className="h-12" 
-                                    src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" 
+                                    src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
                                     alt="Tuple" />
                             </div>
                             <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
