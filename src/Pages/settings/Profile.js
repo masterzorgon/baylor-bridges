080d3acb6920b05600366cad739d54be51f41162
Cloudy Young <cloudy.young@outlook.com>
2022-01-24 05:15:15 -0700

    Add update modal template

--- src/Pages/settings/Profile.js
+++ src/Pages/settings/Profile.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { Fragment, useState, useEffect, useContext } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 import axios from "axios";
 
 import SettingsNavbar from "../../components/SettingsNavbar";
@@ -65,6 +66,7 @@ const profile = {
 const Profile = () => {
     const { getAccountLocal } = useContext(AccountContext);
     const [account, setAccount] = useState(null);
+    const [open, setOpen] = useState(false);
 
     const getValueRaw = (section_key, field) => {
         var account_from = account;
@@ -103,6 +105,7 @@ const Profile = () => {
                 <button
                     type="button"
                     className="bg-white rounded-md font-medium text-emerald-600 hover:text-emerald-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
+                    onClick={() => handleOpenUpdate(section_key, field)}
                 >{text}</button>
             );
         };
@@ -123,6 +126,11 @@ const Profile = () => {
         }
 
         return makeButton("Update");
+    };
+
+    const handleOpenUpdate = (section_key, field) => {
+        console.log(section_key, field);
+        setOpen(true);
     };
 
     useEffect(() => {
@@ -194,6 +202,72 @@ const Profile = () => {
                     </div>
                 </div>
             </div>
+            <Transition.Root show={open} as={Fragment}>
+                <Dialog as="div" className="fixed z-50 inset-0 overflow-y-auto" onClose={setOpen}>
+                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-200"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-150"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-40 transition-opacity" />
+                        </Transition.Child>
+
+                        {/* This element is to trick the browser into centering the modal contents. */}
+                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+                            &#8203;
+                        </span>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enterTo="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                                <div className="sm:flex sm:items-start">
+                                    <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                        
+                                    </div>
+                                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                                            Deactivate account
+                                        </Dialog.Title>
+                                        <div className="mt-2">
+                                            <p className="text-sm text-gray-500">
+                                                Are you sure you want to deactivate your account? All of your data will be permanently removed
+                                                from our servers forever. This action cannot be undone.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="mt-5 sm:mt-4 sm:ml-10 sm:pl-4 sm:flex">
+                                    <button
+                                        type="button"
+                                        className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:w-auto sm:text-sm"
+                                        onClick={() => setOpen(false)}
+                                    >
+                                        Deactivate
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 px-4 py-2 bg-white text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                                        onClick={() => setOpen(false)}
+                                    >
+                                        Cancel
+                                    </button>
+                                </div>
+                            </div>
+                        </Transition.Child>
+                    </div>
+                </Dialog>
+            </Transition.Root>
         </>
     );
 };
