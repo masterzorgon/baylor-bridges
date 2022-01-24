61b122c38958eabf1d15233301e8f9b7adf21d10
Cloudy Young <cloudy.young@outlook.com>
2022-01-24 04:52:17 -0700

    Add remove button to photo

--- src/Pages/settings/Profile.js
+++ src/Pages/settings/Profile.js
@@ -15,7 +15,6 @@ const profile = {
                 title: "Photo",
                 value: "photo",
                 type: "file",
-                button: ["update", "remove"]
             },
             name: {
                 title: "Name",
@@ -89,6 +88,33 @@ const Profile = () => {
         }
     };
 
+    const getButtons = (section_key, field) => {
+        const update = (
+            <button
+                type="button"
+                className="bg-white rounded-md font-medium text-emerald-600 hover:text-emerald-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
+            >Update</button>
+        );
+
+        if (field.value === "photo") {
+            return (
+                <>
+                    <button
+                        type="button"
+                        className="bg-white rounded-md font-medium text-emerald-600 hover:text-emerald-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
+                    >Update</button>
+                    <span className="text-gray-300 mt-2.5" aria-hidden="true">|</span>
+                    <button
+                        type="button"
+                        className="bg-white rounded-md font-medium text-emerald-600 hover:text-emerald-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
+                    >Remove</button>
+                </>
+            );
+        }
+
+        return update;
+    };
+
     useEffect(() => {
         var account = getAccountLocal();
         if (account === null) {
@@ -125,26 +151,21 @@ const Profile = () => {
                                                     <div key={section_key} className="mt-10 divide-y divide-gray-200">
                                                         {/* Title and description */}
                                                         <div className="space-y-1">
-                                                            <h3 className="text-lg leading-6 font-medium text-gray-900">{ section.title }</h3>
-                                                            <p className="max-w-2xl text-sm text-gray-500">{ section.description }</p>
+                                                            <h3 className="text-lg leading-6 font-medium text-gray-900">{section.title}</h3>
+                                                            <p className="max-w-2xl text-sm text-gray-500">{section.description}</p>
                                                         </div>
                                                         <div className="mt-6">
                                                             <dl className="divide-y divide-gray-200">
                                                                 {
                                                                     Object.entries(section.fields).map(([field_key, field]) => (
-                                                                        <div key={field_key} className = "py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4" >
-                                                                            <dt className="text-sm font-medium text-gray-500">{ field.title }</dt>
+                                                                        <div key={field_key} className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4" >
+                                                                            <dt className="text-sm font-medium text-gray-500">{field.title}</dt>
                                                                             <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                                                                                 <span className="flex-grow">
-                                                                                    { getValue(section_key, field) }
+                                                                                    {getValue(section_key, field)}
                                                                                 </span>
-                                                                                <span className="ml-4 flex-shrink-0">
-                                                                                    <button
-                                                                                        type="button"
-                                                                                        className="bg-white rounded-md font-medium text-emerald-600 hover:text-emerald-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
-                                                                                    >
-                                                                                    Update
-                                                                                    </button>
+                                                                                <span className="ml-4 flex-shrink-0 flex item-start space-x-4">
+                                                                                    {getButtons(section_key, field)}
                                                                                 </span>
                                                                             </dd>
                                                                         </div>
