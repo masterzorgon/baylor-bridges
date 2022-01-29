2a649dd44134e22c4d5da14527e79ceb38deff73
Tina <xiayan_li2@baylor.edu>
2022-01-29 10:56:10 +0800

    read dropdowns selected options from database

--- src/Pages/settings/Profile.js
+++ src/Pages/settings/Profile.js
@@ -260,7 +260,13 @@ const Profile = () => {
                             <div>
                                 <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
                                     {/* fixme showing the correct value here */}
-                                    {value.title==="Visibility"?account[value.key]:account["contact_info"][value.key]}
+                                    {
+                                        value.title==="Visibility"?
+                                            account["contact_info"][value.key]
+                                            :
+                                            account[value.key]
+
+                                    }
                                     
                                     <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
                                 </Menu.Button>
