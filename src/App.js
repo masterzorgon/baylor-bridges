d3524ae6c95765c147c5f42420ed10c6d34fc0f8
Tina <xiayan_li2@baylor.edu>
2022-01-29 22:00:04 +0800

    add the template for privacy policies

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import NotFound from "./Pages/404";
 import ContactUs from "./Pages/contactUs";
 import { default as SettingsProfile } from "./Pages/settings/Profile";
 import { Account } from "./components/Account";
+import PrivacyPolicy from "./Pages/policies&terms/PrivacyPolicy";
 
 import "./App.css";
 
@@ -59,6 +60,8 @@ function App() {
                     <Route path="/404" element={components(<Navbar />, <NotFound />, <Footer />)} />
                     <Route path="*" element={<Navigate to="/404" />} />
                     <Route path="/contactUs" element={components(<Navbar/>, <ContactUs/> ,<Footer/>)}/>
+
+                    <Route path="/terms/privacyPolicies" element={components(<Navbar/>,<PrivacyPolicy/>,<Footer/>)}/>
                 </Routes>
             </Router>
         </Account>
