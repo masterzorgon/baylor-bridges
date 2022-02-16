71240c88c27446b7a0c62ccc6ce662241d00b8b1
Cloudy Young <cloudy.young@outlook.com>
2022-02-16 01:10:13 -0700

    Create auth challenge UI

--- src/Pages/signin/Challenge.js
+++ src/Pages/signin/Challenge.js
@@ -6,18 +6,20 @@ import Password from "../../components/Password";
 
 const SignInChallenge = () => {
 
+    const { authChallenge } = useContext(AccountContext);
+
     const [loading, setLoading] = useState(false);
     const [error_message, setErrorMessage] = useState(null);
-    const { signIn } = useContext(AccountContext);
+    const [response, setResponse] = useState(null);
+    const [success, setSuccess] = useState(false);
 
     const urlParams = new URLSearchParams(window.location.search);
     const session = urlParams.get("session");
     const name = urlParams.get("name");
     const sub = urlParams.get("sub");
 
-    console.log(session, name, sub);
+    console.log(name, sub);
 
-    
     const challenges = {
         "NEW_PASSWORD_REQUIRED": {
             form: (
@@ -35,29 +37,51 @@ const SignInChallenge = () => {
                         <Password
                             value={
                                 (password, check) => {
-                                    console.log(password);
+                                    setResponse({ ...response, new_password: password, username: sub });
                                 }
                             }
                         />
                     </div>
                 </div>
             ),
+            success: (
+                <div>
+                    <h3 className="text-lg leading-6 font-medium text-gray-900">
+                        Your new password is set
+                    </h3>
+                    <div className="mt-2 max-w-xl text-sm text-gray-500">
+                        <p>
+                            For future sign in, please use your new password.
+                        </p>
+                    </div>
+                </div>
+            ),
+            submit: () => {
+                console.log(response);
+
+                authChallenge(name, session, response).then(response => {
+                    // window.location.href = "/";
+                    setSuccess(true);
+                }).catch(error => {
+                    setErrorMessage(error.response.data.message);
+                }).finally(() => {
+                    setLoading(false); 
+                });
+            },
+            done: () => {
+                window.location.href = "/";
+            }
         }
     };
 
+    // If it is an undefined challenge, then it is an error (Or could be a challenge have not been implemented yet)
+    if (challenges[name] === undefined) {
+        window.location.href = "/404";   
+    }
+
     const onSubmit = (event) => {
         setLoading(true);
-        signIn()
-            .then(response => {
-                console.log(response);
-                window.location.href = "/";
-            })
-            .catch(error => {
-                setErrorMessage(error.message);
-            })
-            .finally(() => {
-                setLoading(false);
-            });
+        challenges[name].submit();
     };
 
     return (
@@ -95,28 +119,44 @@ const SignInChallenge = () => {
                             }
 
                             {/* Challenge Form */}
-                            {challenges[name].form}
+                            {success === false && challenges[name].form}
+                            {success === true && challenges[name].success}
 
-                            <div>
-                                <button
-                                    type="submit"
-                                    className={`${loading ? "cursor-not-allowed" : ""} w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500`}
-                                    onClick={onSubmit}
-                                    {...(loading ? { disabled: true } : {})}
-                                >
-                                    {
-                                        loading &&
-                                        <svg className="cursor-not-allowed animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
-                                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fillOpacity="0"></circle>
-                                            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                        </svg>
-                                    }
-                                    {
-                                        !loading &&
-                                        "Submit"
-                                    }
-                                </button>
-                            </div>
+                            {
+                                success === false &&
+                                <div>
+                                    <button
+                                        type="submit"
+                                        className={`${loading ? "cursor-not-allowed" : ""} w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500`}
+                                        onClick={onSubmit}
+                                        {...(loading ? { disabled: true } : {})}
+                                    >
+                                        {
+                                            loading &&
+                                            <svg className="cursor-not-allowed animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+                                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fillOpacity="0"></circle>
+                                                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                            </svg>
+                                        }
+                                        {
+                                            !loading &&
+                                            "Submit"
+                                        }
+                                    </button>
+                                </div>
+                            }
+                            {
+                                success === true &&
+                                <div>
+                                    <button
+                                        type="submit"
+                                        className={`${loading ? "cursor-not-allowed" : ""} w-full flex justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-emerald-700 bg-emerald-100 hover:bg-emerald-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500`}
+                                        onClick={challenges[name].done}
+                                    >
+                                        Complete
+                                    </button>
+                                </div>
+                            }
                         </div>
                     </div>
                 </div>
