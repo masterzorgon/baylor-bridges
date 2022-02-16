302ae6a869bcc0aa7ad1193512f46641fa8acd40
Cloudy Young <cloudy.young@outlook.com>
2022-02-16 02:34:30 -0700

    Update 404.js

--- src/Pages/404.js
+++ src/Pages/404.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {ArrowSmRightIcon} from "@heroicons/react/solid";
 
 const NotFound = () => {
     return (
@@ -10,9 +11,10 @@ const NotFound = () => {
                             <p className="text-sm font-semibold text-emerald-600 uppercase tracking-wide">404 error</p>
                             <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">Page not found.</h1>
                             <p className="mt-2 text-base text-gray-500">Sorry, we couldn’t find the page you’re looking for.</p>
-                            <div className="mt-6">
-                                <a href="/" className="text-base font-medium text-emerald-600 hover:text-emerald-500">
-                                    Go back home
+                            <div className="mt-6 w-full grid place-items-center">
+                                <a href="/" className="text-base font-medium text-emerald-600 hover:text-emerald-500 flex items-center space-x-0.5">
+                                    <span>Go back home</span>
+                                    <ArrowSmRightIcon className="h-4 w-4" />
                                 </a>
                             </div>
                         </div>
