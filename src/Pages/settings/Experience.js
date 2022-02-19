3b42e1dfb193fb6ebe2736e4383f7506d9b92e58
Tina <xiayan_li2@baylor.edu>
2022-02-19 23:12:00 +0800

    replace experience card template

--- src/Pages/settings/Experience.js
+++ src/Pages/settings/Experience.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,Fragment } from "react";
 import SettingsNavbar from "../../components/SettingsNavbar";
 // eslint-disable-next-line no-unused-vars
 import DatePicker from "tailwind-react-datepicker";
+import { Menu, Transition } from "@headlessui/react";
+import { PencilIcon, DotsVerticalIcon, DocumentRemoveIcon } from "@heroicons/react/solid";
 
 import axios from "axios";
 const Experience=()=>{
@@ -37,6 +39,10 @@ const Experience=()=>{
     };
 
 
+    function classNames(...classes) {
+        return classes.filter(Boolean).join(" ");
+    }
+      
     return(
         <>
             <div>
@@ -56,98 +62,90 @@ const Experience=()=>{
                                             <div className="space-y-10 sm:px-6 lg:px-0 lg:col-span-9">
                                                 { experiences.map((exper,idx)=> (
 
-                                                
-                                                    <section aria-labelledby="payment-details-heading" key={exper.exper_id}>
-                                                        <form action="#" method="POST">
-                                                            <div className="shadow sm:rounded-md sm:overflow-hidden">
-                                                                <div className="bg-white py-6 px-4 sm:p-6">
-                                                                    <div>
-                                                                        <h2 id="payment-details-heading" className="text-lg leading-6 font-medium text-gray-900">
-                          Experience
-                                                                        </h2>
-                                                                        <p className="mt-1 text-sm text-gray-500">
-                          Update your current experience. Notice that it may take up to 5 mins to update on search functionality
+                                                    <>
+                                                        <section aria-labelledby="payment-details-heading" key={exper.exper_id}>
+                                                            <form action="#" method="POST">
+                                                                <div className="shadow sm:rounded-md ">
+                                                                    <div className="bg-white py-2 px-2 sm:p-2">
+
+                                                                        <div className="bg-white px-4 py-5 sm:px-6">
+                                                                            <div className="flex space-x-3">
+                                                                                <div className="min-w-0 flex-1">
+                                                                                    <h1 className="text-md font-medium text-gray-900">
+                                                                                        
+                                                                                        {exper.title}
+                                                                                    </h1>
+                                                                                    <h2 className="text-sm text-gray-500">
+                                                                                        from {exper.start_time} to {exper.stop_time}
+                                                                                    </h2>
+                                                                                </div>
+                                                                                <div className="flex-shrink-0 self-center flex">
+                                                                                    <Menu as="div" className="relative z-30 inline-block text-left">
+                                                                                        <div>
+                                                                                            <Menu.Button className="-m-2 p-2 rounded-full flex items-center text-gray-400 hover:text-gray-600">
+                                                                                                <span className="sr-only">Open options</span>
+                                                                                                <DotsVerticalIcon className="h-5 w-5" aria-hidden="true" />
+                                                                                            </Menu.Button>
+                                                                                        </div>
+
+                                                                                        <Transition
+                                                                                            as={Fragment}
+                                                                                            enter="transition ease-out duration-100"
+                                                                                            enterFrom="transform opacity-0 scale-95"
+                                                                                            enterTo="transform opacity-100 scale-100"
+                                                                                            leave="transition ease-in duration-75"
+                                                                                            leaveFrom="transform opacity-100 scale-100"
+                                                                                            leaveTo="transform opacity-0 scale-95"
+                                                                                        >
+                                                                                            <Menu.Items className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                                                                <div className="py-1">
+                                                                                                    <Menu.Item>
+                                                                                                        {({ active }) => (
+                                                                                                            <a
+                                                                                                                href="#"
+                                                                                                                className={classNames(
+                                                                                                                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                                                                                                                    "flex px-4 py-2 text-sm"
+                                                                                                                )}
+                                                                                                            >
+                                                                                                                <PencilIcon className="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+                                                                                                                <span>Edit</span>
+                                                                                                            </a>
+                                                                                                        )}
+                                                                                                    </Menu.Item>
+                                                                                                    <Menu.Item>
+                                                                                                        {({ active }) => (
+                                                                                                            <a
+                                                                                                                href="#"
+                                                                                                                className={classNames(
+                                                                                                                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                                                                                                                    "flex px-4 py-2 text-sm"
+                                                                                                                )}
+                                                                                                            >
+                                                                                                                <DocumentRemoveIcon className="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+                                                                                                                <span>Remove</span>
+                                                                                                            </a>
+                                                                                                        )}
+                                                                                                    </Menu.Item>
+                                                                                                </div>
+                                                                                            </Menu.Items>
+                                                                                        </Transition>
+                                                                                    </Menu>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+
+
+                                                                    </div>
+                                                                    <div className="bg-white py-6 px-4 sm:p-6">
+                                                                        <p className="block text-sm font-medium text-gray-600">
+                                                                            {exper.description}
                                                                         </p>
                                                                     </div>
 
-                                                                    <div className="mt-6 grid grid-cols-4 gap-6">
-                                                                   
-
-                                                                        <div className="col-span-4 sm:col-span-2">
-                                                                            <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
-                                                                            Title
-                                                                            </label>
-                                                                            <input
-                                                                                type="text"
-                                                                                name="email-address"
-                                                                                id="email-address"
-                                                                                autoComplete="email"
-                                                                                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
-                                                                                value={exper.title}
-                                                                                onChange={(e)=> handleChange(idx,"title",e.target.value)}
-                                                                            />
-                                                                        </div>
-
-
-                                                                        
-
-                                                                        <div className="col-span-4 sm:col-span-1">
-                                                                            <label htmlFor="expiration-date" className="block text-sm font-medium text-gray-700">
-                                                                                Start date
-                                                                            </label>
-                                                                            <input
-                                                                                type="date"
-                                                                                name="expiration-date"
-                                                                                id="expiration-date"
-                                                                                autoComplete="cc-exp"
-                                                                                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
-                                                                                placeholder="MM / YY"
-                                                                            />
-                                                                        </div>
-                                                                        <div className="col-span-4 sm:col-span-1">
-                                                                            <label htmlFor="expiration-date" className="block text-sm font-medium text-gray-700">
-                                                                                End date
-                                                                            </label>
-                                                                            <input
-                                                                                type="text"
-                                                                                name="expiration-date"
-                                                                                id="expiration-date"
-                                                                                autoComplete="cc-exp"
-                                                                                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
-                                                                                placeholder="MM / YY"
-                                                                            />
-                                                                        </div>
-
-
-                                                                    </div>
                                                                 </div>
-                                                                <div className="bg-white py-6 px-4 sm:p-6">
-                                                                    <label htmlFor="comment" className="block text-sm font-medium text-gray-700">
-                                                                description
-                                                                    </label>
-                                                                    <div className="mt-1">
-                                                                        <textarea
-                                                                            rows={4}
-                                                                            name="comment"
-                                                                            id="comment"
-                                                                            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                                                                            defaultValue={""}
-                                                                            value={exper.description}
-                                                                            onChange={(e)=> handleChange(idx,"description",e.target.value)}
-                                                                        />
-                                                                    </div>
-                                                                </div>
-                                                                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                                                                    <button
-                                                                        type="submit"
-                                                                        className="bg-gray-800 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-                                                                    >
-                                                                    Save
-                                                                    </button>
-                                                                </div>
-                                                            </div>
-                                                        </form>
-                                                    </section>
+                                                            </form>
+                                                        </section></>
                                                 ))}
                                             </div>
                                         </div>
