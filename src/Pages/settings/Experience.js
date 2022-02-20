91c60433c0abf7b245569f6a0be35df1988b4590
Tina <xiayan_li2@baylor.edu>
2022-02-20 11:40:22 +0800

    add edit experience card

--- src/Pages/settings/Experience.js
+++ src/Pages/settings/Experience.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState,Fragment } from "react";
 import SettingsNavbar from "../../components/SettingsNavbar";
 // eslint-disable-next-line no-unused-vars
 import DatePicker from "tailwind-react-datepicker";
-import { Menu, Transition } from "@headlessui/react";
+import { Menu, Transition,Dialog } from "@headlessui/react";
 import { PencilIcon, DotsVerticalIcon, DocumentRemoveIcon } from "@heroicons/react/solid";
+
 
 import axios from "axios";
 const Experience=()=>{
     const [experiences,setExperiences]=useState([]);
     const [update,setUpdate]=useState(false);
+    const [open,setOpen]=useState(false);
 
     useEffect(()=>{
         
@@ -35,6 +37,11 @@ const Experience=()=>{
         console.log("updating experience is ",new_exper[index]);
         setExperiences(new_exper);
         setUpdate(true);
+
+    };
+
+    // eslint-disable-next-line no-unused-vars
+    const getModal =(request) =>{
 
     };
 
@@ -102,11 +109,12 @@ const Experience=()=>{
                                                                                                     <Menu.Item>
                                                                                                         {({ active }) => (
                                                                                                             <a
-                                                                                                                href="#"
+                                                                                                                
                                                                                                                 className={classNames(
                                                                                                                     active ? "bg-gray-100 text-gray-900" : "text-gray-700",
                                                                                                                     "flex px-4 py-2 text-sm"
                                                                                                                 )}
+                                                                                                                onClick={()=> setOpen(true)}
                                                                                                             >
                                                                                                                 <PencilIcon className="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
                                                                                                                 <span>Edit</span>
@@ -156,6 +164,125 @@ const Experience=()=>{
                     </div>
                 </div>
             </div>
+
+            <Transition.Root show={open} as={Fragment}>
+                <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={setOpen}>
+                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                        </Transition.Child>
+
+                        {/* This element is to trick the browser into centering the modal contents. */}
+                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+            &#8203;
+                        </span>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enterTo="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-xl sm:w-full sm:p-6">
+                                <section aria-labelledby="payment-details-heading">
+                                    <form action="#" method="POST">
+                                        <div className="shadow sm:rounded-md sm:overflow-hidden">
+                                            <div className="bg-white py-6 px-4 sm:p-6">
+                                                <div>
+                                                    <h2 id="payment-details-heading" className="text-lg leading-6 font-medium text-gray-900">
+                                                        Edit Experience
+                                                    </h2>
+                                                    
+                                                </div>
+
+                                                <div className="mt-6 grid grid-cols-4 gap-6">
+                                                    <div className="col-span-4 sm:col-span-2">
+                                                        <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
+                                                                            Title
+                                                        </label>
+                                                        <input
+                                                            type="text"
+                                                            name="email-address"
+                                                            id="email-address"
+                                                            autoComplete="email"
+                                                            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
+
+                                                        />
+                                                    </div>
+
+                                                    <div className="col-span-4 sm:col-span-1">
+                                                        <label htmlFor="start-date" className="block text-sm font-medium text-gray-700">
+                                                            Start Date
+                                                        </label>
+                                                        <input
+                                                            type="text"
+                                                            name="expiration-date"
+                                                            id="expiration-date"
+                                                            autoComplete="cc-exp"
+                                                            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
+                                                            placeholder="MM / YY"
+                                                        />
+                                                        {/* <DatePicker/> */}
+                                                    </div>
+                                                    <div className="col-span-4 sm:col-span-1">
+                                                        <label htmlFor="expiration-date" className="block text-sm font-medium text-gray-700">
+                                                            End date
+                                                        </label>
+                                                        <input
+                                                            type="text"
+                                                            name="expiration-date"
+                                                            id="expiration-date"
+                                                            autoComplete="cc-exp"
+                                                            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
+                                                            placeholder="MM / YY"
+                                                        />
+                                                    </div>
+
+                                                    <div className="col-span-4 sm:col-span-4">
+                                                        <label htmlFor="comment" className="block text-sm font-medium text-gray-700">
+                                                                description
+                                                        </label>
+                                                        
+                                                        <textarea
+                                                            rows={4}
+                                                            name="comment"
+                                                            id="comment"
+                                                            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 py-2 px-3 block w-full sm:text-sm border-gray-300 rounded-md"
+                                                            defaultValue={""}
+                                                        />
+                                                        
+                                                    </div>
+                                                    
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                    </form>
+                                </section>
+                                <div className="mt-5 sm:mt-6">
+                                    <button
+                                        type="button"
+                                        className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+                                        onClick={() => setOpen(false)}
+                                    >
+                                        update this experience
+                                    </button>
+                                </div>
+                            </div>
+                        </Transition.Child>
+                    </div>
+                </Dialog>
+            </Transition.Root>
         </>    
     );
 
