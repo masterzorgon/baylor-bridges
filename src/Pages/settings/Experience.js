69132a7cef837c72eaccfdcdc338fdd19000c6c8
Tina <xiayan_li2@baylor.edu>
2022-02-21 11:20:04 +0800

    enable update exper to database

--- src/Pages/settings/Experience.js
+++ src/Pages/settings/Experience.js
@@ -55,6 +55,20 @@ const Experience=()=>{
 
             
         };
+
+        const handleSubmit=(field)=>{
+            let url="/account/profile/experience/"+field["exper_id"];
+            console.log(url);
+            axios.put(url,field).then(res=>{
+                console.log("update the experience successfully");
+                setOpen(false);                
+            });
+            
+
+
+            // TODO: also submit publications changes
+        };
+
         console.log("in get modal the field is ",field);
 
         if(!field){
@@ -65,7 +79,7 @@ const Experience=()=>{
             return(
                 <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-xl sm:w-full sm:p-6">
                     <section aria-labelledby="payment-details-heading">
-                        <form action="#" method="POST">
+                        <div>
                             <div className="shadow sm:rounded-md sm:overflow-hidden">
                                 <div className="bg-white py-6 px-4 sm:p-6">
                                     <div>
@@ -145,17 +159,19 @@ const Experience=()=>{
                                 </div>
                                             
                             </div>
-                        </form>
-                    </section>
-                    <div className="mt-5 sm:mt-6">
-                        <button
-                            type="button"
-                            className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
-                            onClick={() => setOpen(false)}
-                        >
+
+                            <div className="mt-5 sm:mt-6">
+                                <button
+                                    type="submit"
+                                    className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+                                    onClick={() => handleSubmit(field)}
+                                >
                                         update this experience
-                        </button>
-                    </div>
+                                </button>
+                            </div>
+                        </div>
+                    </section>
+                    
                 </div>
             );
         }else if(request==="remove"){
@@ -196,7 +212,7 @@ const Experience=()=>{
 
                                                     <>
                                                         <section aria-labelledby="payment-details-heading" key={exper.exper_id}>
-                                                            <form action="#" method="POST">
+                                                            <form>
                                                                 <div className="shadow sm:rounded-md ">
                                                                     <div className="bg-white py-2 px-2 sm:p-2">
 
