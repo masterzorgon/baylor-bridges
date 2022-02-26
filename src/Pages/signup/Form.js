ecc64f2fa8b914280c6972c4ac20e2f97e74af79
Cloudy Young <cloudy.young@outlook.com>
2022-02-26 00:27:46 -0700

    Update Form.js

--- src/Pages/signup/Form.js
+++ src/Pages/signup/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MailIcon, ArrowSmRightIcon } from "@heroicons/react/outline";
 import { useParams } from "react-router-dom";
 import axios from "axios";
@@ -9,6 +9,7 @@ import Password from "../../components/Password";
 const Form = () => {
     const [currentStep, setCurrentStep] = useState(1);
     const [loading, setLoading] = useState(false);
+    const [complete, setComplete] = useState(false);
     const [step, setStep] = useState(1);
 
     const [email, setEmail] = useState("");
@@ -21,16 +22,29 @@ const Form = () => {
         window.location.href = "student";
     }
 
+    useEffect(() => {
+        if (step === 1) {
+            let reg = /^\w+([-+.'][^\s]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+            let is_valid = reg.test(email || "");
+
+            setComplete(email && email !== "" && is_valid);
+        }
+    }, [step, email]);
+
     const checkEmail = () => {
         setLoading(true);
-        axios.get("localhost:5000/signup/email/" + email).then(res => {
+        axios.get("/signup/email/" + email).then(res => {
             console.log(res);
+        }).catch(err => {
+
+        }).finally(() => {
+            setLoading(false);
         });
     };
 
     const step1 = () => {
         return (
-            <div className="mt-10 px-5 md:mt-2 md:bg-white md:shadow md:rounded-lg md:px-8 md:py-8 md:-mx-8">
+            <>
                 <h3 className="text-lg leading-6 font-medium text-gray-900">Your email address</h3>
                 <p className="mt-1 text-sm font-medium mb-4 text-gray-500">{role === "student" ? "Please use your Baylor University email to sign up as a current student." : "Please use your email address to sign up as an alumnus." }</p>
                 <div className="mt-4 relative rounded-md shadow-sm">
@@ -47,28 +61,7 @@ const Form = () => {
                         onChange={(e) => setEmail(e.target.value)}
                     />
                 </div>
-
-                <div className="mt-6 text-sm text-right w-full grid place-items-end">
-                    <button
-                        type="button"
-                        className="relative text-center text-sm px-4 py-2 border border-transparent font-medium rounded-sm shadow-sm text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
-                        onClick={checkEmail}
-                        disabled={loading}
-                    >
-                        {
-                            loading &&
-                            <svg className="absolute pointer-events-none animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
-                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fillOpacity="0"></circle>
-                                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                            </svg>
-                        }
-                        <span className={`flex items-center ${loading ? "invisible" : ""}`}>
-                            <span>Next</span>
-                            <ArrowSmRightIcon className="h-4 w-4" />
-                        </span>
-                    </button>
-                </div>
-            </div>
+            </>
         );
     };
 
@@ -76,48 +69,24 @@ const Form = () => {
     const step2 = () => {
         return (
             <>
-                <div className="mt-10 px-5 md:mt-2 md:bg-white md:shadow md:rounded-lg md:px-8 md:py-8 md:-mx-8">
-                    <h3 className="text-lg leading-6 font-medium text-gray-900">Your password</h3>
-                    <p className="mt-1 text-sm font-medium mb-4 text-gray-500">Set a password for your account.</p>
-                    <Password
-                        value={(password, check) => setPassword(password)}
+                <h3 className="text-lg leading-6 font-medium text-gray-900">Your password</h3>
+                <p className="mt-1 text-sm font-medium mb-4 text-gray-500">Set a password for your account.</p>
+                <Password
+                    value={(password, check) => setPassword(password)}
+                />
+
+                {/* <div className="mt-4 relative rounded-md shadow-sm">
+                    <label htmlFor="confirm-password" className="block text-sm font-medium text-gray-700">
+                        Confirm password
+                    </label>
+                    <input
+                        id="confirm-password"
+                        name="confirm-password"
+                        type="password"
+                        autoComplete="password"
+                        className="mt-1 py-2 px-3 block w-full shadow-sm focus:ring-emerald-500 focus:border-emerald-500 border-gray-300 rounded-md"
                     />
-
-                    {/* <div className="mt-4 relative rounded-md shadow-sm">
-                        <label htmlFor="confirm-password" className="block text-sm font-medium text-gray-700">
-                            Confirm password
-                        </label>
-                        <input
-                            id="confirm-password"
-                            name="confirm-password"
-                            type="password"
-                            autoComplete="password"
-                            className="mt-1 py-2 px-3 block w-full shadow-sm focus:ring-emerald-500 focus:border-emerald-500 border-gray-300 rounded-md"
-                        />
-                    </div> */}
-
-                    <div className="mt-6 text-sm text-right w-full grid place-items-end">
-                        <button
-                            type="button"
-                            className="text-sm flex items-center space-x-0.5 px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
-                        >
-                            {
-                                loading &&
-                                <svg className="pointer-events-none animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
-                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fillOpacity="0"></circle>
-                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                </svg>
-                            }
-                            {
-                                !loading &&
-                                <>
-                                    <span>Next</span>
-                                    <ArrowSmRightIcon className="h-4 w-4" />
-                                </>
-                            }
-                        </button>
-                    </div>
-                </div>
+                </div> */}
             </>
         );
     };
@@ -146,17 +115,41 @@ const Form = () => {
                 {/* Title and subtitle */}
                 <div className="text-center">
                     <img
-                        className="mx-auto h-16 w-auto"
+                        className="mx-auto h-9 w-auto"
                         src="/Baylor-University-Athletics-01.svg"
                         alt="Workflow"
                     />
-                    <h2 className="mt-2 text-2xl font-extrabold tracking-tight text-gray-900 sm:text-2xl">Sign up</h2>
+                    <h2 className="mt-2 text-lg font-extrabold tracking-tight text-gray-900 sm:text-2xl">Sign up</h2>
                 </div>
 
                 <Progress currentStep={step} />
 
-                { step === 1 && step1() }
-                { step === 2 && step2() }
+
+                <div className="px-5 md:mt-2 md:bg-white md:shadow md:rounded-lg md:px-8 md:py-8 md:-mx-8">
+                    {step === 1 && step1()}
+                    {step === 2 && step2()}
+
+                    <div className="mt-6 text-sm text-right w-full grid place-items-end">
+                        <button
+                            type="button"
+                            className="relative text-center text-sm px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:opacity-50 disabled:cursor-not-allowed"
+                            onClick={checkEmail}
+                            disabled={loading || !complete}
+                        >
+                            {
+                                loading &&
+                                <svg className="absolute pointer-events-none animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fillOpacity="0"></circle>
+                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                </svg>
+                            }
+                            <span className={`flex items-center ${loading ? "invisible" : ""}`}>
+                                <span>Next</span>
+                                <ArrowSmRightIcon className="h-4 w-4" />
+                            </span>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
