18f9c6d6b6b57d4f724d2037bed72ac0b1a431a2
Cloudy Young <cloudy.young@outlook.com>
2022-02-25 22:51:14 -0700

    Update Profile.js

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -41,7 +41,7 @@ const Profile = () => {
                             <div className="flex-shrink-0">
                                 <div className="relative">
                                     <Photo size="16" />
-                                    <span className="absolute inset-0 shadow-inner rounded-full" aria-hidden="true" />
+                                    <span className="absolute inset-0 shadow-inner rounded-full h-16 w-16" aria-hidden="true" />
                                 </div>
                             </div>
                             {
