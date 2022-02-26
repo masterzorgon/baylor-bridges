0e64e00780b292386cad4052bdbe01be2dcd0fcd
JosephYu1 <Joseph_Yu1@baylor.edu>
2022-02-26 01:25:03 -0600

    change search_bar_profile_pic src/components/Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
 }
 
-const avatar="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80";
+//const avatar="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80";
 // const people = [
 //     {
 //         name: "Calvin Hawkins",
@@ -223,7 +223,8 @@ const Navbar = (props) => {
                                             <li key={person.email}>
                                                 <a  className="py-4 px-5 flex hover:bg-gray-50" href={"/profile/"+person.user_id} target="_blank" rel="noreferrer">
                                                     {/* TODO adding account avatar later*/}
-                                                    <img className="h-10 w-10 rounded-full" src={avatar} alt="" />
+                                                    {/*<img className="h-10 w-10 rounded-full" src={avatar} alt="" />*/}
+                                                    <Photo size="10" />
                                                     <div className="ml-3">
                                                         <p className="text-sm font-medium text-gray-900">{person.first_name} {person.last_name}</p>
                                                         <p className="text-sm text-gray-500">{person.occupation}</p>
