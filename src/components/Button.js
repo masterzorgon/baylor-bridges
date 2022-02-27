0c108a94302d7942bc55a3907cbacf3e40dc4ce2
Cloudy Young <cloudy.young@outlook.com>
2022-02-27 09:05:07 -0700

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,7 +9,7 @@ const Button = ({onClick, loading, disabled, children, className}) =>{
     return (
         <button
             type="button"
-            className={classNames("relative text-center w-full flex justify-center text-sm px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:opacity-50 disabled:cursor-not-allowed", className)}
+            className={classNames("text-center w-full flex justify-center text-sm px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:opacity-50 disabled:cursor-not-allowed", className)}
             onClick={onClick}
             disabled={disabled}
         >
