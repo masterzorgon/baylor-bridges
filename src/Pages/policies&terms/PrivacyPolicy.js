de52db2a83b2732bb8d2a9b9005ed26822e15f50
Cloudy Young <cloudy.young@outlook.com>
2022-02-27 22:30:43 -0700

    Fix useState bug

--- src/Pages/policies&terms/PrivacyPolicy.js
+++ src/Pages/policies&terms/PrivacyPolicy.js
@@ -1,6 +1,4 @@
-import React, { useEffect } from "react";
-import { useState } from "react/cjs/react.development";
-
+import React, { useEffect, useState } from "react";
 
 
 export default function PrivacyPolicy() {
