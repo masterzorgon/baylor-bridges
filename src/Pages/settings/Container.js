4f0689e01aa261a4aae2bf6580921fdbf4a38e1a
Cloudy Young <cloudy.young@outlook.com>
2022-03-06 04:10:35 -0700

    Create Container.js

---
+++ src/Pages/settings/Container.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Navbar from "./Navbar";
+
+const Container = ({ current, children }) => {
+    return (
+        <>
+            <div className="max-w-4xl mx-auto flex flex-col md:px-8 xl:px-0">
+                <main className="flex-1">
+                    <div className="relative max-w-4xl mx-auto md:px-8 xl:px-0">
+                        <div className="pt-10 pb-16">
+                            <div className="px-4 sm:px-6 md:px-0">
+                                <h1 className="text-3xl font-extrabold text-gray-900">Settings</h1>
+                            </div>
+                            <div className="px-4 sm:px-6 md:px-0">
+                                <div className="py-6">
+                                    <Navbar current={current} />
+                                    {children}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </>
+    );
+};
+
+export default Container;
