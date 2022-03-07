629963cde7cf54cc5844ee5f78625ea763acfd5e
Cloudy Young <cloudy.young@outlook.com>
2022-03-06 22:01:03 -0700

    Remove commented out code

--- src/Pages/settings/Navbar.js
+++ src/Pages/settings/Navbar.js
@@ -3,9 +3,6 @@ import React from "react";
 const classNames = (...classes) => classes.filter(Boolean).join(" ");
 
 const Navbar = (props) => {
-
-    // const [selection, setSelection] = React.useState("");
-
     const tabs = [
         { id: "account", name: "Account", href: "account" },
         { id: "profile", name: "Profile", href: "profile" },
@@ -13,41 +10,8 @@ const Navbar = (props) => {
         // { id: "workload", name: "Workload", href: "workload" },
     ];
 
-    /*
-    const handleRedirect = (event) => {
-        const selectedTab = document.getElementById("selected-tab");
-        const selectedValue = selectedTab.options[selectedTab.selectedIndex].value;
-        
-        window.location.href = selectedValue;
-
-        // setSelection(event.value);
-    };
-    */
-
     return (
         <>
-            {/* 
-                FIX:
-                    - the default value in selection menu does not change upon selection
-            */}
-
-            {/* MOBILE VIEW: SELECT MENU */}
-            {/* 
-            <nav className="lg:hidden">
-                <select
-                    id="selected-tab"
-                    name="selected-tab"
-                    className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm rounded-md"
-                    // defaultValue={selection} // tabs.find((tab) => tab.id === props.current).name
-                    onChange={handleRedirect}
-                >
-                    {tabs.map((tab) => (
-                        <option value={tab.href} key={tab.name}>{tab.name}</option>
-                    ))}
-                </select>
-            </nav>
-            */}
-
             {/* DESKTOP VIEW: */}
             <div className="sm:block lg:block">
                 <div className="border-b border-gray-200">
