bebe3ecbc9472e4586efb58d2535e42decb19d50
Nathan <nathan.zebedee@gmail.com>
2022-03-06 21:34:32 -0600

    Edits header to Contact Us

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -347,7 +347,7 @@ const About = () => {
                         src="https://www2.baylor.edu/wp-content/uploads/2017/05/patneff-may17.jpg"
                         alt=""
                     />
-                    <div className="absolute inset-0 bg-emerald-800 mix-blend-multiply" aria-hidden="true" />
+                    <div className="absolute inset-0 bg-emerald-700 mix-blend-multiply" aria-hidden="true" />
                 </div>
                 <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
                     <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">
