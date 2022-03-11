92cc0b64ba1cbb3f0d8dfe431cca7839bc53c186
Cloudy Young <cloudy.young@outlook.com>
2022-03-11 16:50:23 -0700

    Update Photo.js

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -22,38 +22,39 @@ const Photo = ({ size, account }) => {
     }
 
 
-    if (account === null) {
-        return <div className={`inline-flex items-center justify-center h-${size} w-${size} rounded-full bg-gray-500`}>
-            <span className={`text-${font_size} font-medium leading-none text-white capitalize`}></span>
-        </div>;
+    if ("photo" in account) {
+        return (
+            <img
+                className={`h-${size} w-${size} rounded-full`}
+                src={account.photo}
+                alt=""
+            />
+        );
     }
 
-    if (!("photo" in account)) {
-        let initials = "";
-        if ("first_name" in account && account.first_name) {
-            initials += account.first_name.charAt(0);
-        }
+    
+    let initials = "";
 
-        if ("last_name" in account && account.last_name) {
-            initials += account.last_name.charAt(0);
-        }
-
-        // If has no name for this account, take the first letter of the email
-        if (initials === "" && "email" in account && account.email) {
-            initials += account.email.charAt(0);
-        }
-
-
-        return <div className={`inline-flex items-center justify-center h-${size} w-${size} rounded-full bg-gray-500`}>
-            <span className={`text-${font_size} font-medium leading-none text-white capitalize`}>{initials}</span>
-        </div>;
-    } else {
-        return <img
-            className={`h-${size} w-${size} rounded-full`}
-            src={account.photo}
-            alt=""
-        />;
+    if ("first_name" in account && account.first_name) {
+        initials += account.first_name.charAt(0);
     }
+
+    if ("last_name" in account && account.last_name) {
+        initials += account.last_name.charAt(0);
+    }
+
+    // If has no name for this account, take the first letter of the email
+    if (initials === "" && "email" in account && account.email) {
+        initials += account.email.charAt(0);
+    }
+
+    return (
+        <div className={`h-${size} w-${size}`}>
+            <div className={`inline-flex items-center justify-center h-${size} w-${size} rounded-full bg-gray-500`}>
+                <span className={`text-${font_size} font-medium leading-none text-white capitalize`}>{initials}</span>
+            </div>
+        </div>
+    );
 };
 
 export default Photo;
