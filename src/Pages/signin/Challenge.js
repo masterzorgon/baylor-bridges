0e55093260d3b94cc34fe4e714de767fb6248e9b
Cloudy Young <cloudy.young@outlook.com>
2022-03-13 19:00:54 -0600

    Replace primary button with the component one

--- src/Pages/signin/Challenge.js
+++ src/Pages/signin/Challenge.js
@@ -3,6 +3,7 @@ import { XCircleIcon } from "@heroicons/react/solid";
 
 import { AccountContext } from "../../components/Account";
 import Password from "../../components/Password";
+import Button from "../../components/Button";
 
 const Challenge = () => {
 
@@ -126,24 +127,13 @@ const Challenge = () => {
                             {
                                 success === false &&
                                 <div>
-                                    <button
-                                        type="submit"
-                                        className={`${loading ? "cursor-not-allowed" : ""} w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:opacity-50 disabled:cursor-not-allowed`}
+                                    <Button
                                         onClick={onSubmit}
+                                        loading={loading}
                                         disabled={loading || !complete}
                                     >
-                                        {
-                                            loading &&
-                                            <svg className="cursor-not-allowed animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
-                                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fillOpacity="0"></circle>
-                                                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                            </svg>
-                                        }
-                                        {
-                                            !loading &&
-                                            "Submit"
-                                        }
-                                    </button>
+                                        Submit
+                                    </Button>
                                 </div>
                             }
                             {
--- src/Pages/signup/Form.js
+++ src/Pages/signup/Form.js
@@ -6,6 +6,7 @@ import axios from "axios";
 
 import Progress from "./Progress";
 import Password from "../../components/Password";
+import Button from "../../components/Button";
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
@@ -288,24 +289,17 @@ const Form = () => {
 
 
                     <div className="mt-6 text-sm text-right w-full grid place-items-end">
-                        <button
-                            type="button"
+                        <Button
                             className="relative text-center text-sm px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:opacity-50 disabled:cursor-not-allowed"
                             onClick={onSubmit}
+                            loading={loading}
                             disabled={loading || !complete}
                         >
-                            {
-                                loading &&
-                                <svg className="absolute animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
-                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fillOpacity="0"></circle>
-                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                </svg>
-                            }
                             <span className={`flex items-center ${loading ? "invisible" : ""}`}>
                                 <span>Next</span>
                                 <ArrowSmRightIcon className="h-4 w-4" />
                             </span>
-                        </button>
+                        </Button>
                     </div>
                 </div>
             </div>
