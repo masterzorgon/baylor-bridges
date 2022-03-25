ec40abc4bb5aa48ea23116723eaf9a04997f1969
nathanzebedee <nathan.zebedee@gmail.com>
2022-03-24 23:01:35 -0500

    Current version

--- src/Pages/signin/setup/EnterName.js
+++ src/Pages/signin/setup/EnterName.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
 import { UserCircleIcon } from "@heroicons/react/outline";
+import { SortAscendingIcon } from "@heroicons/react/solid";
+import axios from "axios";
 
 
 const EnterName = () => {
@@ -11,7 +13,7 @@ const EnterName = () => {
         {
             name: "Name",
             description:
-                "This is your full, legal name. This is the name others will know you by via your Baylor Bridges account.",
+                "Please provide your full, legal name. This is the name others will know you by via your Baylor Bridges account.",
             icon: UserCircleIcon,
         },
         // {
@@ -29,6 +31,29 @@ const EnterName = () => {
         //     icon: NewspaperIcon,
         // },
     ];
+
+    const onSubmit = event => {
+        setShow(show => !show);
+
+        axios.get("/account/profile") // account
+            .then(res => {
+                console.log("---RESPONSE---", res.data);
+                
+            })
+            .catch(err => console.log("---ERROR---", err));
+        
+        // CODE FOR UPDATING USER PROFILE
+        // axios.put("/account/profile", account)
+        //     .then(res => {
+        //         console.log(res);
+        //         console.log(account);
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //     });
+        
+        // window.location.href = "/sign-in/setup/all-done";
+    };  
 
     return (
         <div className="bg-white">
@@ -60,7 +85,7 @@ const EnterName = () => {
             >
                 <div className="grid grid-cols-1 gap-y-20 lg:gap-y-0 lg:gap-x-8">
                     {supportLinks.map((link) => (
-                        <div key={link.name} className="flex flex-col bg-white rounded-2xl shadow-xl">
+                        <div key={link.name} className="flex flex-col bg-gray-50 rounded-2xl shadow-xl">
                             <div className="flex-1 relative pt-16 px-6 pb-8 md:px-8">
 
                                 <Transition
@@ -80,10 +105,33 @@ const EnterName = () => {
                                 <h3 className="text-xl font-medium text-gray-900">{link.name}</h3>
                                 <p className="mt-4 text-base text-gray-500">{link.description}</p>
                             </div>
+                            
                             <div className="p-6 bg-gray-50 rounded-bl-2xl rounded-br-2xl md:px-8">
-                                <button onClick={() => setShow(show => !show)}>
-                                    show
-                                </button>
+                                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                                    Search candidates
+                                </label>
+                                <div className="mt-1 flex rounded-md shadow-sm">
+                                    <div className="relative flex items-stretch flex-grow focus-within:z-10">
+                                        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                            <link.icon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                        </div>
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            id="email"
+                                            className="focus:ring-emerald-500 focus:border-emerald-500 block w-full rounded-none rounded-l-md pl-10 sm:text-sm border-gray-300"
+                                            placeholder="John Doe"
+                                        />
+                                    </div>
+                                    <button
+                                        type="button"
+                                        onClick={onSubmit}
+                                        className="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-emerald-500 focus:border-emerald-500"
+                                    >
+                                        <SortAscendingIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                        <span>Submit</span>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     ))}
