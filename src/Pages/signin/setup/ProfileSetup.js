c07f62c90d0d40b19f7435784c30fb3e3988f163
nathanzebedee <nathan.zebedee@gmail.com>
2022-03-28 18:02:23 -0500

    Animates buttons on InputInfo

--- src/Pages/signin/setup/ProfileSetup.js
+++ src/Pages/signin/setup/ProfileSetup.js
@@ -1,6 +1,6 @@
-import React, { Fragment, useRef, useCallback } from "react"; 
-import { Popover, Transition } from "@headlessui/react";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import React, { useRef, useCallback } from "react"; 
+// import { Popover, Transition } from "@headlessui/react";
+// import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import ReactCanvasConfetti from "react-canvas-confetti";
 
 const ProfileSetup = () => {
@@ -69,7 +69,6 @@ const ProfileSetup = () => {
 
     return (
         <div className="relative bg-white-50 overflow-hidden">
-            <ReactCanvasConfetti refConfetti={getInstance} style={canvasStyles} />
             <div className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full" aria-hidden="true">
                 <div className="relative h-full max-w-7xl mx-auto">
                     <svg
@@ -117,93 +116,49 @@ const ProfileSetup = () => {
                 </div>
             </div>
 
-            <div className="relative pt-6 pb-16 sm:pb-24">
-                <Popover>
-                    <div className="max-w-7xl mx-auto px-4 sm:px-6">
-                        <nav className="relative flex items-center justify-between sm:h-10 md:justify-center" aria-label="Global">
-                            <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
-                                <div className="flex items-center justify-between w-full md:w-auto">
-                                    <a href="/sign-in">
-                                        <img
-                                            className="h-8 w-auto sm:h-10"
-                                            src="/favicon.ico"
-                                            alt=""
-                                        />
-                                    </a>
-                                    <div className="-mr-2 flex items-center md:hidden">
-                                        <Popover.Button className="bg-gray-50 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-emerald-500">
-                                            <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                                        </Popover.Button>
-                                    </div>
+            <div className="relative sm:mx-10 sm:my-10 flex justify-center align-middle">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-6">
+                    <div className="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
+                        <div className="absolute inset-0">
+                            <img
+                                className="h-full w-full object-cover"
+                                src="/landing_page_background.jfif"
+                                alt="People working on laptops"
+                            />
+                            <div className="absolute inset-0 bg-emerald-700 mix-blend-multiply" />
+                            <ReactCanvasConfetti refConfetti={getInstance} style={canvasStyles} />
+                        </div>
+                        <div className="relative px-4 py-14 sm:px-6 sm:py-24 lg:py-28 lg:px-8">
+                            <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
+                                <span className="block text-white">
+                                    Welcome,
+                                </span>
+                                <span className="block text-emerald-200">
+                                    We are so glad to have you!
+                                </span>
+                            </h1>
+                            <p className="mt-6 max-w-lg mx-auto text-center text-xl text-white sm:max-w-3xl">
+                                Since your account is so new, we will walk you through
+                                a few steps to get you started. You will be prompted to fill out
+                                some profile information so that others can better connect with you
+                                via your Baylor Bridges account.
+                            </p>
+                            <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
+                                <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-1 sm:gap-5">
+                                    <button
+                                        href="/sign-up"
+                                        onClick={onSubmit}
+                                        className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-emerald-700 bg-white sm:px-9
+                                            transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-103 hover:bg-emerald-100 duration-200"
+                                    >
+                                        Get Started
+                                    </button>
                                 </div>
-                            </div>
-                        </nav>
-                    </div>
-
-                    <Transition
-                        as={Fragment}
-                        enter="duration-150 ease-out"
-                        enterFrom="opacity-0 scale-95"
-                        enterTo="opacity-100 scale-100"
-                        leave="duration-100 ease-in"
-                        leaveFrom="opacity-100 scale-100"
-                        leaveTo="opacity-0 scale-95"
-                    >
-                        <Popover.Panel
-                            focus
-                            className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-                        >
-                            <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                                <div className="px-5 pt-4 flex items-center justify-between">
-                                    <div>
-                                        <img
-                                            className="h-8 w-auto"
-                                            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                                            alt=""
-                                        />
-                                    </div>
-                                    <div className="-mr-2">
-                                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-emerald-500">
-                                            <span className="sr-only">Close menu</span>
-                                            <XIcon className="h-6 w-6" aria-hidden="true" />
-                                        </Popover.Button>
-                                    </div>
-                                </div>
-                                <a
-                                    href="#"
-                                    className="block w-full px-5 py-3 text-center font-medium text-emerald-600 bg-gray-50 hover:bg-gray-100"
-                                >
-                                    Log in
-                                </a>
-                            </div>
-                        </Popover.Panel>
-                    </Transition>
-                </Popover>
-
-                <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
-                    <div className="text-center">
-                        <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                            <span className="block xl:inline">Welcome fellow Bear!</span>{" "}
-                            <span className="block text-emerald-600 xl:inline">Let&apos;s setup your account.</span>
-                        </h1>
-                        <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-                            Since your account is so new, we will walk you through
-                            a few steps to get you started. You will be prompted to fill out
-                            some profile information so that others can better connect with you
-                            via your Baylor Bridges account.
-                        </p>
-                        <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
-                            <div className="rounded-md shadow">
-                                <button
-                                    onClick={onSubmit}
-                                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-emerald-600 hover:bg-emerald-700 md:py-4 md:text-lg md:px-10"
-                                >
-                                    Get started
-                                </button>
                             </div>
                         </div>
                     </div>
-                </main>
+                </div>
+                <div className="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100 -z-10 lg:hidden"></div>
             </div>
         </div>
     );
