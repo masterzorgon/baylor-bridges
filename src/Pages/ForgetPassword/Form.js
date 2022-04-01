e30f4a82c961f0e93176317a0f76cf12b76e5990
Tina <xiayan_li2@baylor.edu>
2022-04-01 20:24:21 +0800

    fix the style of forgot password form

--- src/Pages/ForgetPassword/Form.js
+++ src/Pages/ForgetPassword/Form.js
@@ -194,22 +194,6 @@ const Form = () => {
     return (
         <div className="bg-white py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
             <div className="relative max-w-xl mx-auto">
-                <svg className="absolute left-full transdiv translate-x-1/2" width={404} height={404} fill="none" viewBox="0 0 404 404" aria-hidden="true">
-                    <defs>
-                        <pattern id="85737c0e-0916-41d7-917f-596dc7edfa27" x={0} y={0} width={20} height={20} patternUnits="userSpaceOnUse">
-                            <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-                        </pattern>
-                    </defs>
-                    <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
-                </svg>
-                <svg className="absolute right-full bottom-0 transdiv -translate-x-1/2" width={404} height={404} fill="none" viewBox="0 0 404 404" aria-hidden="true">
-                    <defs>
-                        <pattern id="85737c0e-0916-41d7-917f-596dc7edfa27" x={0} y={0} width={20} height={20} patternUnits="userSpaceOnUse">
-                            <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-                        </pattern>
-                    </defs>
-                    <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
-                </svg>
 
                 {/* Title and subtitle */}
                 <div className="text-center">
@@ -226,7 +210,7 @@ const Form = () => {
                 <Progress currentStep={step} steps={steps} />
 
 
-                <div className="px-5 mt-10 md:mt-2 md:bg-white md:shadow md:rounded-lg md:px-8 md:py-8 md:-mx-8">
+                <div className="bg-white mt-10 py-8 px-4 shadow sm:rounded-lg sm:px-10 sm:mt-5">
 
                     {step === 1 && step1()}
                     {step === 2 && step2()}
