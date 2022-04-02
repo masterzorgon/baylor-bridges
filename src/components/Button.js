87f715a9adbe01349bc0d322cdaef7b87eda3317
Cloudy Young <cloudy.young@outlook.com>
2022-04-01 19:49:58 -0600

    Update button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,12 +3,12 @@ import React from "react";
 import { classNames } from "./Utils";
 
 
-const Button = ({onClick, loading, disabled, children, className}) => {
+const Button = ({onClick, loading, disabled, children, className, arrow}) => {
 
     return (
         <button
             type="button"
-            className={classNames("text-center w-full flex justify-center px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:opacity-50 disabled:cursor-not-allowed relative", className)}
+            className={classNames("btn text-center w-full flex justify-center px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:opacity-50 disabled:cursor-not-allowed relative", className)}
             onClick={onClick}
             disabled={disabled}
         >
@@ -19,11 +19,21 @@ const Button = ({onClick, loading, disabled, children, className}) => {
                 </svg>
             </div>
 
-            <div className={classNames("transition-all", loading ? "opacity-0" : "")}>
+            <div className={classNames("transition-all flex items-center", loading ? "opacity-0" : "")}>
                 {children}
+                {
+                    arrow === true &&
+                    <Arrow />
+                }
             </div>
 
         </button>
+    );
+};
+
+const Arrow = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" className="octicon arrow-symbol" width="16" height="16" viewBox="0 0 16 16" fill="none"><path fill="currentColor" d="M7.28033 3.21967C6.98744 2.92678 6.51256 2.92678 6.21967 3.21967C5.92678 3.51256 5.92678 3.98744 6.21967 4.28033L7.28033 3.21967ZM11 8L11.5303 8.53033C11.8232 8.23744 11.8232 7.76256 11.5303 7.46967L11 8ZM6.21967 11.7197C5.92678 12.0126 5.92678 12.4874 6.21967 12.7803C6.51256 13.0732 6.98744 13.0732 7.28033 12.7803L6.21967 11.7197ZM6.21967 4.28033L10.4697 8.53033L11.5303 7.46967L7.28033 3.21967L6.21967 4.28033ZM10.4697 7.46967L6.21967 11.7197L7.28033 12.7803L11.5303 8.53033L10.4697 7.46967Z"></path><path stroke="currentColor" d="M1.75 8H11" strokeWidth="1.5" strokeLinecap="round"></path></svg>
     );
 };
 
