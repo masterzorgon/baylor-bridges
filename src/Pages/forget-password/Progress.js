bbe73779d7746326de7fe21f04ad3d389357bb83
Tina <xiayan_li2@baylor.edu>
2022-04-02 12:07:13 +0800

    change the color to yellow for unverified step

--- src/Pages/forget-password/Progress.js
+++ src/Pages/forget-password/Progress.js
@@ -15,7 +15,7 @@ const Progress = ({ currentStep, steps }) => {
         <div>
             <div className="mt-6" aria-hidden="true">
                 <div className="bg-gray-200 rounded-full overflow-hidden">
-                    <div className="h-2 bg-emerald-800 rounded-full" style={{ width: percentProgress() }} />
+                    <div className={`h-2 ${steps[currentStep - 1].isSent ? "bg-emerald-800" : "bg-yellow-500"} rounded-full`} style={{ width: percentProgress() }} />
                 </div>
                 <div className="hidden sm:grid grid-cols-4 text-sm font-medium text-gray-600 mt-6">
                     {steps.map((step) => (
