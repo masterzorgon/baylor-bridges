d9e846a6101bb08bbbb5d7c830e50345ad348721
nathanzebedee <nathan.zebedee@gmail.com>
2022-04-08 09:21:14 -0500

    Fixed modal vanishing bug

--- src/Pages/signin/setup/InfoInput.js
+++ src/Pages/signin/setup/InfoInput.js
@@ -48,54 +48,8 @@ const InfoInput = () => {
 
     return (
         <>
-            <div className="flex-col justify-center">
+            <div className="flex-col justify-center sm:overflow-hidden">
                 {/* Header */}
-                <div className="relative sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full pointer-events-none -z-10" aria-hidden="true">
-                    <div className="relative h-full w-full mx-auto overflow-hidden">
-                        <svg
-                            className="flex right-full transform md:-translate-y-1/4 -translate-y-1/4 -translate-x-3/4"
-                            width={404}
-                            height={784}
-                            fill="none"
-                            viewBox="0 0 404 784"
-                        >
-                            <defs>
-                                <pattern
-                                    id="f210dbf6-a58d-4871-961e-36d5016a0f49"
-                                    x={0}
-                                    y={0}
-                                    width={20}
-                                    height={20}
-                                    patternUnits="userSpaceOnUse"
-                                >
-                                    <rect x={0} y={0} width={4} height={4} className="text-gray-300" fill="currentColor" />
-                                </pattern>
-                            </defs>
-                            <rect width={404} height={784} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
-                        </svg>
-                        <svg
-                            className="relative left-full transform -translate-y-1/2 -translate-x-1/4"
-                            width={404}
-                            height={784}
-                            fill="none"
-                            viewBox="0 0 404 784"
-                        >
-                            <defs>
-                                <pattern
-                                    id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
-                                    x={0}
-                                    y={0}
-                                    width={20}
-                                    height={20}
-                                    patternUnits="userSpaceOnUse"
-                                >
-                                    <rect x={0} y={0} width={4} height={4} className="text-gray-300" fill="currentColor" />
-                                </pattern>
-                            </defs>
-                            <rect width={404} height={784} fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)" />
-                        </svg>
-                    </div>
-                </div>
                 <div className="flex-col mb-5 pb-32">
                     <div className="absolute inset-0">
                         <img
@@ -104,6 +58,52 @@ const InfoInput = () => {
                             alt=""
                         />
                         <div className="absolute inset-0 bg-gray-200 mix-blend-multiply" aria-hidden="true" />
+                        <div className="relative sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full pointer-events-none -z-10" aria-hidden="true">
+                            <div className="relative h-full w-full mx-auto overflow-hidden">
+                                <svg
+                                    className="flex right-full transform -translate-y-1/4 -translate-x-3/4"
+                                    width={404}
+                                    height={784}
+                                    fill="none"
+                                    viewBox="0 0 404 784"
+                                >
+                                    <defs>
+                                        <pattern
+                                            id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                                            x={0}
+                                            y={0}
+                                            width={20}
+                                            height={20}
+                                            patternUnits="userSpaceOnUse"
+                                        >
+                                            <rect x={0} y={0} width={4} height={4} className="text-gray-300" fill="currentColor" />
+                                        </pattern>
+                                    </defs>
+                                    <rect width={404} height={784} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+                                </svg>
+                                <svg
+                                    className="relative left-full transform -translate-y-1/2 -translate-x-1/4"
+                                    width={404}
+                                    height={784}
+                                    fill="none"
+                                    viewBox="0 0 404 784"
+                                >
+                                    <defs>
+                                        <pattern
+                                            id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
+                                            x={0}
+                                            y={0}
+                                            width={20}
+                                            height={20}
+                                            patternUnits="userSpaceOnUse"
+                                        >
+                                            <rect x={0} y={0} width={4} height={4} className="text-gray-300" fill="currentColor" />
+                                        </pattern>
+                                    </defs>
+                                    <rect width={404} height={784} fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)" />
+                                </svg>
+                            </div>
+                        </div>
                     </div>
                     <div className="relative max-w-7xl mx-auto px-4 py-10 sm:px-8 lg:px-8">
                         <h1 className="text-4xl font-extrabold tracking-tight text-white md:text-5xl lg:text-6xl">
