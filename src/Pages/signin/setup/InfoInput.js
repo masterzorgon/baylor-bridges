6b38a88b4e5ad31e7bf73df088cae05a277f0ba9
nathanzebedee <nathan.zebedee@gmail.com>
2022-04-07 22:21:53 -0500

    Centers page and limits amount page can expand

--- src/Pages/signin/setup/InfoInput.js
+++ src/Pages/signin/setup/InfoInput.js
@@ -38,38 +38,40 @@ const InfoInput = () => {
     useEffect(() => console.log("ACCOUNT", account), [account]);
 
     const modals = () => {
-        if (modal === 1) { return <NameInput        account={account} setAccount={setAccount} show={show} modal={modal} setShow={setShow} setModal={setModal} />; }
-        if (modal === 2) { return <ContactInput     account={account} setAccount={setAccount} show={show} modal={modal} setShow={setShow} setModal={setModal} />; }
-        if (modal === 3) { return <LocationInput    account={account} setAccount={setAccount} show={show} modal={modal} setShow={setShow} setModal={setModal} />; }
-        if (modal === 4) { return <GradInput        account={account} setAccount={setAccount} show={show} modal={modal} setShow={setShow} setModal={setModal} />; }
-        if (modal === 5) { return <HeadlineInput    account={account} setAccount={setAccount} show={show} modal={modal} setShow={setShow} setModal={setModal} />; }
-        if (modal === 6) { return <AllDone          account={account} setAccount={setAccount} show={show} modal={modal} setShow={setShow} setModal={setModal} />; }
+        if (modal === 1) { return <NameInput        account={account} setAccount={setAccount} show={show}  setShow={setShow} modal={modal} setModal={setModal} />; }
+        if (modal === 2) { return <ContactInput     account={account} setAccount={setAccount} show={show}  setShow={setShow} modal={modal} setModal={setModal} />; }
+        if (modal === 3) { return <LocationInput    account={account} setAccount={setAccount} show={show}  setShow={setShow} modal={modal} setModal={setModal} />; }
+        if (modal === 4) { return <GradInput        account={account} setAccount={setAccount} show={show}  setShow={setShow} modal={modal} setModal={setModal} />; }
+        if (modal === 5) { return <HeadlineInput    account={account} setAccount={setAccount} show={show}  setShow={setShow} modal={modal} setModal={setModal} />; }
+        if (modal === 6) { return <AllDone          account={account} setAccount={setAccount} show={show}  setShow={setShow} modal={modal} setModal={setModal} />; }
     };
 
     return (
         <>
-            <div className="bg-white">
-                {/* Header */}
-                <div className="relative mb-5 pb-32 bg-gray-800">
-                    <div className="absolute inset-0">
-                        <img
-                            className="w-full h-full object-cover"
-                            src="https://upload.wikimedia.org/wikipedia/commons/d/d6/Old_Main%2C_Baylor_University.jpg"
-                            alt=""
-                        />
-                        <div className="absolute inset-0 bg-gray-500 mix-blend-multiply" aria-hidden="true" />
+            <div className="flex justify-center 3xl:w-80">
+                <div className="bg-white">
+                    {/* Header */}
+                    <div className="relative mb-5 pb-32 bg-gray-800">
+                        <div className="absolute inset-0">
+                            <img
+                                className="w-full h-full object-cover"
+                                src="https://upload.wikimedia.org/wikipedia/commons/d/d6/Old_Main%2C_Baylor_University.jpg"
+                                alt=""
+                            />
+                            <div className="absolute inset-0 bg-gray-500 mix-blend-multiply" aria-hidden="true" />
+                        </div>
+                        <div className="relative max-w-7xl mx-auto px-4 py-10 sm:px-6 lg:px-8">
+                            <h1 className="text-4xl font-extrabold tracking-tight text-white md:text-5xl lg:text-6xl">
+                                Enter your profile information
+                            </h1>
+                            <p className="mt-6 max-w-3xl text-xl text-gray-300">
+                                The information you provide here should be authentic and verifiable.
+                            </p>
+                        </div>
                     </div>
-                    <div className="relative max-w-7xl mx-auto px-4 py-10 sm:px-6 lg:px-8">
-                        <h1 className="text-4xl font-extrabold tracking-tight text-white md:text-5xl lg:text-6xl">
-                            Enter your profile information
-                        </h1>
-                        <p className="mt-6 max-w-3xl text-xl text-gray-300">
-                            The information you provide here should be authentic and verifiable.
-                        </p>
-                    </div>
+                    {/* Overlapping cards */}
+                    {modals()}
                 </div>
-                {/* Overlapping cards */}
-                {modals()}
             </div>
         </>
     );
