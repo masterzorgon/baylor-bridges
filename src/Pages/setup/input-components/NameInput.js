0a3cac847e894df65373b3d1365a209c1c37686d
Cloudy Young <cloudy.young@outlook.com>
2022-04-12 03:45:07 -0600

    Update NameInput.js

--- src/Pages/setup/input-components/NameInput.js
+++ src/Pages/setup/input-components/NameInput.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from "react";
 import { Transition } from "@headlessui/react";
-import { UserCircleIcon, ArrowLeftIcon, ArrowRightIcon, ExclamationCircleIcon, XIcon } from "@heroicons/react/outline";
+import { UserCircleIcon } from "@heroicons/react/outline";
 import { useTimeoutFn } from "react-use";
 import { useTransition, animated } from "react-spring";
+
+import Button from "../../../components/Button";
 
 const NameInput = ({ account, setAccount, modal, show, setModal, setShow }) => {
 
@@ -43,7 +45,7 @@ const NameInput = ({ account, setAccount, modal, show, setModal, setShow }) => {
                 leaveTo="opacity-0"
             >
                 <section
-                    className="-mt-32 max-w-7xl sm:mx-20 relative z-10 pb-32 px-4 sm:px-6 lg:px-8"
+                    className=""
                     aria-labelledby="contact-heading"
                 >
                     {/* ALERT NOTIFICATION BELOW */}
@@ -67,7 +69,7 @@ const NameInput = ({ account, setAccount, modal, show, setModal, setShow }) => {
                                     <div className="p-4">
                                         <div className="flex items-start">
                                             <div className="flex-shrink-0">
-                                                <ExclamationCircleIcon className="h-6 w-6 text-red-400" aria-hidden="true" />
+                                                {/* <ExclamationCircleIcon className="h-6 w-6 text-red-400" aria-hidden="true" /> */}
                                             </div>
                                             <div className="ml-1 w-0 flex-1 pt-0.5">
                                                 <p className="text-sm font-medium text-gray-900">Submission unsuccessful</p>
@@ -81,7 +83,7 @@ const NameInput = ({ account, setAccount, modal, show, setModal, setShow }) => {
                                                     }}
                                                 >
                                                     <span className="sr-only">Close</span>
-                                                    <XIcon className="h-5 w-5" aria-hidden="true" />
+                                                    {/* <XIcon className="h-5 w-5" aria-hidden="true" /> */}
                                                 </button>
                                             </div>
                                         </div>
@@ -91,14 +93,15 @@ const NameInput = ({ account, setAccount, modal, show, setModal, setShow }) => {
                         </div>
                     </div>
                     {/* ALERT NOTIFICATION ABOVE */}
-                    <div className="lg:w-3/4 xl:1/2 grid grid-cols-1 gap-y-20 lg:gap-y-0 lg:gap-x-8 mx-auto">
-                        <div className="flex flex-col bg-white rounded-2xl shadow-xl">
+
+                    <div className="grid grid-cols-1 gap-y-20 lg:gap-y-0 lg:gap-x-8 mx-auto">
+                        <div className="flex flex-col bg-white rounded-2xl">
                             <div className="flex-1 relative pt-16 px-6 pb-8 md:px-8">
 
                                 {transition((style, item) => {
                                     return item
                                         ?
-                                        <animated.div style={style} className="absolute top-0 p-5 inline-block bg-emerald-600 rounded-xl shadow-xl transform -translate-y-1/2">
+                                        <animated.div style={style} className="absolute top-0 p-5 inline-block bg-emerald-600 rounded-xl shadow-md transform -translate-y-1/2">
                                             <UserCircleIcon className="h-6 w-6 text-white" aria-hidden="true" />
                                         </animated.div>
                                         : "";
@@ -109,79 +112,54 @@ const NameInput = ({ account, setAccount, modal, show, setModal, setShow }) => {
                             </div>
                             <div className="p-6 pt-0 bg-white rounded-bl-2xl rounded-br-2xl md:px-8">
                                 <div className="isolate -space-y-px rounded-md shadow-sm">
-                                    <div className="relative border border-gray-300 rounded-md rounded-b-none px-3 py-2 focus-within:z-10 focus-within:ring-1 focus-within:ring-emerald-600 focus-within:border-emerald-600">
-                                        <div className="flex justify-between">
-                                            <label htmlFor="name" className="block text-xs font-medium text-gray-900">
-                                                First Name
-                                            </label>
-                                            <span className="text-sm text-gray-500" id="email-optional">
-                                                Required
-                                            </span>
-                                        </div>
+                                    <div className="relative border border-gray-300 rounded-md rounded-b-none px-3 py-2 focus-within:z-10 focus-within:ring-1 focus-within:ring-emerald-600 focus-within:border-emerald-600 transition-colors">
+                                        <label htmlFor="first-name" className="block text-xs font-medium text-gray-900">
+                                            First Name
+                                        </label>
                                         <input
                                             type="text"
                                             name="first-name"
                                             id="first-name"
                                             className="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
-                                            placeholder="Jane"
+                                            placeholder=""
+                                            autoComplete="off"
                                             onChange={event => setAccount({ ...account, first_name: event.target.value })}
                                             value={account.first_name}
                                         />
                                     </div>
-                                    <div className="relative border border-gray-300 rounded-md rounded-t-none px-3 py-2 focus-within:z-10 focus-within:ring-1 focus-within:ring-emerald-600 focus-within:border-emerald-600">
-                                        <div className="flex justify-between">
-                                            <label htmlFor="name" className="block text-xs font-medium text-gray-900">
-                                                Last Name
-                                            </label>
-                                            <span className="text-sm text-gray-500" id="email-optional">
-                                                Required
-                                            </span>
-                                        </div>
+                                    <div className="relative border border-gray-300 rounded-md rounded-t-none px-3 py-2 focus-within:z-10 focus-within:ring-1 focus-within:ring-emerald-600 focus-within:border-emerald-600 transition-colors">
+                                        <label htmlFor="last-name" className="block text-xs font-medium text-gray-900">
+                                            Last name
+                                        </label>
                                         <input
                                             type="text"
                                             name="last-name"
                                             id="last-name"
                                             className="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
-                                            placeholder="Doe"
+                                            placeholder=""
+                                            autoComplete="off"
                                             onChange={event => setAccount({ ...account, last_name: event.target.value })}
                                             value={account.last_name}
                                         />
                                     </div>
                                 </div>
 
-                                <div className="flex justify-between">
-                                    <button
-                                        type="button"
+                                <div className="flex mt-6 space-x-2">
+                                    <Button
                                         onClick={prevModal}
-                                        className="mt-6 inline-flex items-center px-4 py-2 border border-emerald-600 shadow-sm text-sm font-medium rounded-md text-emerald-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500
-                                        transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-200 hover:shadow-md"
+                                        className="px-4 py-3 border shadow-sm text-sm bg-gray-100 font-medium rounded-md text-gray-600 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
                                     >
-                                        <ArrowLeftIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
                                         Back
-                                    </button>
-                                    {
-                                        account.first_name === "" || account.last_name === "" ?
-                                            <button
-                                                type="button"
-                                                disabled={true}
-                                                id="next-button"
-                                                className="bg-gray-400 hover:bg-gray-500 disabled:cursor-not-allowed mt-6 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
-                                            >
-                                                Next
-                                                <ArrowRightIcon className="ml-2 -mr-1 h-5 w-5" aria-hidden="true" />
-                                            </button> :
-                                            <button
-                                                type="button"
-                                                onClick={onSubmit}
-                                                id="next-button"
-                                                className=" mt-6 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500
-                                                transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-200 hover:shadow-md"
-                                            >
-                                                Next
-                                                <ArrowRightIcon className="ml-2 -mr-1 h-5 w-5" aria-hidden="true" />
-                                            </button>
+                                    </Button>
 
-                                    }
+                                    <Button
+                                        className="py-3 text-sm"
+                                        disabled={account.first_name === "" || account.last_name === ""}
+                                        onClick={onSubmit}
+                                        arrow={true}
+                                    >
+                                        Next
+                                    </Button>
                                 </div>
                             </div>
                         </div>
