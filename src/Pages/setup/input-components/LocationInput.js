61169cac9dfd7b1b4898646bc7363e4ced61dbea
Cloudy Young <cloudy.young@outlook.com>
2022-04-12 09:59:45 -0600

    Fix set state

--- src/Pages/setup/input-components/LocationInput.js
+++ src/Pages/setup/input-components/LocationInput.js
@@ -1,7 +1,7 @@
-import React, { Fragment, useState } from "react";
-import { LocationMarkerIcon, SelectorIcon } from "@heroicons/react/outline";
+import React, { Fragment, useState, useEffect } from "react";
+import { LocationMarkerIcon, SelectorIcon, CheckIcon } from "@heroicons/react/outline";
 import { useTimeoutFn } from "react-use";
-import { Menu, Transition } from "@headlessui/react";
+import { Listbox, Transition } from "@headlessui/react";
 import { useTransition, animated } from "react-spring";
 
 import Button from "../../../components/Button";
@@ -10,7 +10,10 @@ import { states, classNames } from "../../../components/Utils";
 const LocationInput = ({ account, setAccount, modal, show, setModal, setShow }) => {
 
     const [, , takeAwayModal] = useTimeoutFn(() => setShow(false), 0);
-    const [location, setLocation] = useState(states[0].title);
+
+    const [state, setState] = useState(states[0]);
+    // const [city, setCity] = useState("");
+
     const transition = useTransition(show, { // used to fade icon in
         from: { x: 0, y: 50, opacity: 0 },
         enter: { x: 0, y: -30, opacity: 1 },
@@ -29,10 +32,9 @@ const LocationInput = ({ account, setAccount, modal, show, setModal, setShow }) 
         setTimeout(() => setModal(modal - 1), 400);
     };
 
-    const handleState = (event) => {
-        setLocation(event.target.textContent);
-        setAccount({ ...account, state: event.target.textContent });
-    };
+    useEffect(() => {
+        setAccount({ ...account, state: state.value });
+    }, [state, setAccount]);
 
     return (
         <>
@@ -81,44 +83,60 @@ const LocationInput = ({ account, setAccount, modal, show, setModal, setShow }) 
                                                 Required
                                             </span>
                                         </div>
-                                        <Menu as="div" id="dropdown" className="flex relative text-left z-50">
-                                            <div className="w-full">
-                                                <Menu.Button className="inline-flex justify-betweem w-full rounded-md border border-transparent bg-white text-sm font-medium text-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-white">
-                                                    {location}
-                                                    <SelectorIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-                                                </Menu.Button>
-                                            </div>
-                                            <Transition
-                                                as={Fragment}
-                                                enter="transition ease-out duration-100"
-                                                enterFrom="transform opacity-0 scale-95"
-                                                enterTo="transform opacity-100 scale-100"
-                                                leave="transition ease-in duration-75"
-                                                leaveFrom="transform opacity-100 scale-100"
-                                                leaveTo="transform opacity-0 scale-95"
-                                            >
-                                                <Menu.Items className="overflow-scroll max-h-40 origin-top-right absolute right-0 mt-1 w-full rounded-md shadow-md bg-gray-50 ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                                    <div className="py-1">
-                                                        {states.map((state, index) => (
-                                                            <Menu.Item key={index}>
-                                                                {({ active }) => (
-                                                                    <button
-                                                                        type="submit"
-                                                                        className={classNames(
-                                                                            active ? "bg-gray-100 text-emerald-600" : "text-gray-900",
-                                                                            "block w-full text-left px-4 py-2 text-sm"
+                                        <Listbox value={state} onChange={setState}>
+                                            {({ open }) => (
+                                                <>
+                                                    <div className="mt-1 relative">
+                                                        <Listbox.Button className="bg-white relative w-full rounded-md text-left py-1 cursor-default focus:outline-none focus:ring-0 sm:text-sm">
+                                                            <span className="block truncate">{state.title}</span>
+                                                            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                                                                <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                                            </span>
+                                                        </Listbox.Button>
+
+                                                        <Transition
+                                                            show={open}
+                                                            as={Fragment}
+                                                            leave="transition ease-in duration-100"
+                                                            leaveFrom="opacity-100"
+                                                            leaveTo="opacity-0"
+                                                        >
+                                                            <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                                                                {states.map((state) => (
+                                                                    <Listbox.Option
+                                                                        key={state.value}
+                                                                        className={({ active }) => classNames(
+                                                                            active ? "text-white bg-emerald-600" : "text-gray-900",
+                                                                            "cursor-default select-none relative py-2 pl-3 pr-9"
                                                                         )}
-                                                                        onClick={handleState}
+                                                                        value={state}
                                                                     >
-                                                                        {state.title}
-                                                                    </button>
-                                                                )}
-                                                            </Menu.Item>
-                                                        ))}
+                                                                        {({ selected, active }) => (
+                                                                            <>
+                                                                                <span className={classNames(selected ? "font-semibold" : "font-normal", "block truncate")}>
+                                                                                    {state.title}
+                                                                                </span>
+
+                                                                                {selected ? (
+                                                                                    <span
+                                                                                        className={classNames(
+                                                                                            active ? "text-white" : "text-emerald-600",
+                                                                                            "absolute inset-y-0 right-0 flex items-center pr-4"
+                                                                                        )}
+                                                                                    >
+                                                                                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                                                    </span>
+                                                                                ) : null}
+                                                                            </>
+                                                                        )}
+                                                                    </Listbox.Option>
+                                                                ))}
+                                                            </Listbox.Options>
+                                                        </Transition>
                                                     </div>
-                                                </Menu.Items>
-                                            </Transition>
-                                        </Menu>
+                                                </>
+                                            )}
+                                        </Listbox>
                                     </div>
                                     <div className="relative border border-gray-300 rounded-md rounded-t-none px-3 py-2 focus-within:z-2 focus-within:ring-1 focus-within:ring-emerald-600 focus-within:border-emerald-600">
                                         <label htmlFor="city" className="block text-xs font-medium text-gray-900">
