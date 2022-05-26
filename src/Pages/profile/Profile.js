c5108cb21f80729b1de4d22778b2a1b4a8817fe7
Cloudy Young <cloudy.young@outlook.com>
2022-05-25 23:03:41 -0600

    Update profile experience display

--- src/Pages/profile/Profile.js
+++ src/Pages/profile/Profile.js
@@ -134,14 +134,14 @@ const Profile = () => {
 
     const formatValue = (value, type) => {
         switch (type) {
-        case "email":
-            return <a className="underline underline-offset-4 decoration-dashed decoration-gray-500" href={`mailto:${value}`}>{value}</a>;
+            case "email":
+                return <a className="underline underline-offset-4 decoration-dashed decoration-gray-500" href={`mailto:${value}`}>{value}</a>;
 
-        case "phone":
-            return <a className="underline underline-offset-4 decoration-dashed decoration-gray-500" href={`tel:${value}`}>{value}</a>;
+            case "phone":
+                return <a className="underline underline-offset-4 decoration-dashed decoration-gray-500" href={`tel:${value}`}>{value}</a>;
 
-        default:
-            return value;
+            default:
+                return value;
         }
     };
 
@@ -187,14 +187,20 @@ const Profile = () => {
     };
 
     const getDisplayDateRange = (start, end) => {
+        start = getFormattedDate(start);
+        end = getFormattedDate(end);
+
         let display_date = "";
-        if (start) display_date += getFormattedDate(start);
+        if (start) display_date += start;
         if (start && end) display_date += " - ";
-        if (end) display_date += getFormattedDate(end);
+        if (end) display_date += end;
         return display_date;
     };
 
     const getFormattedDate = (date) => {
+        if (!date) return null;
+        if (date === "present") return "Present";
+
         let d = dayjs(date);
         return d.isValid() ? d.format("MMMM YYYY") : "";
     };
@@ -392,45 +398,51 @@ const Profile = () => {
                                                 <ul className="divide-y">
                                                     {
                                                         profileAccount && profileAccount.experiences && profileAccount.experiences.map((experience, index) => (
-                                                            <li className="px-4 sm:px-6 py-5" key={experience.exper_id}>
-                                                                <p className="font-medium">{experience.title}</p>
-                                                                {
-                                                                    (experience.start_time || experience.stop_time) &&
-                                                                    <p className="font-medium text-sm text-gray-500 mt-0.5">
-                                                                        {getDisplayDateRange(experience.start_time, experience.stop_time)}
-                                                                    </p>
-                                                                }
-                                                                {
-                                                                    experience.description &&
-                                                                    <p className="mt-2 text-sm text-gray-700">{experience.description}</p>
-                                                                }
-
-                                                                {/* PUBLICATIONS */}
-                                                                {
-                                                                    experience.publications && Array.isArray(experience.publications) && experience.publications.length > 0 &&
-                                                                    <div className="mt-4">
-                                                                        <ul className="border border-gray-200 rounded-md divide-y divide-gray-200">
-                                                                            {
-                                                                                experience.publications.map((publication, index) => (
-                                                                                    <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm" key={publication.pub_id}>
-                                                                                        <div className="w-0 flex-1 flex items-center">
-                                                                                            <PaperClipIcon className="flex-shrink-0 h-5 w-5 text-gray-400" />
-                                                                                            <span className="ml-2 flex-1 w-0 truncate text-gray-700">
-                                                                                                {publication.title}
-                                                                                            </span>
-                                                                                        </div>
-                                                                                        <div className="ml-4 flex-shrink-0">
-                                                                                            <a href={/^http:\/\//.test(publication.duo_link) || /^https:\/\//.test(publication.duo_link) ? publication.duo_link : "//" + publication.duo_link}
-                                                                                                className="font-medium text-emerald-600 hover:text-emerald-500" target="_blank" rel="noreferrer">
-                                                                                                Open
-                                                                                            </a>
-                                                                                        </div>
-                                                                                    </li>
-                                                                                ))
-                                                                            }
-                                                                        </ul>
+                                                            <li className="px-4 sm:px-6 py-5" key={index}>
+                                                                <section>
+                                                                    <div className="flex space-x-3">
+                                                                        <div className="flex-shrink-0">
+                                                                            <Photo size="10" />
+                                                                        </div>
+                                                                        <div className="min-w-0 flex-1">
+                                                                            <h1 className="text-md font-medium text-gray-800 -mt-0.5">
+                                                                                {experience.title}
+                                                                            </h1>
+                                                                            <h2 className="text-sm text-gray-500">
+                                                                                {getDisplayDateRange(experience.start_time, experience.stop_time)}
+                                                                            </h2>
+                                                                        </div>
                                                                     </div>
-                                                                }
+
+                                                                    <div className="mt-4 ml-12 pl-1 space-y-4">
+                                                                        <div className="">
+                                                                            <p className="block text-sm font-medium text-gray-600">
+                                                                                {experience.description?.length > 0 ? experience.description : <span className="text-gray-400">This experience has no description.</span>}
+                                                                            </p>
+                                                                        </div>
+
+                                                                        {
+                                                                            experience.publications?.length > 0 &&
+                                                                            <ul className="border border-gray-200 rounded-md divide-y divide-gray-200">
+                                                                                {
+                                                                                    experience.publications.map((publication, pub_index) => (
+                                                                                        <li className="px-3 py-1 flex items-center justify-between text-sm" key={pub_index}>
+                                                                                            <div className="w-0 flex-1 flex items-center">
+                                                                                                <PaperClipIcon className="flex-shrink-0 h-5 w-5 text-gray-400" />
+                                                                                                <span className="ml-2 flex-1 w-0 truncate text-gray-700">
+                                                                                                    <a href={/^http:\/\//.test(publication.duo_link) || /^https:\/\//.test(publication.duo_link) ? publication.duo_link : "//" + publication.duo_link}
+                                                                                                        className="font-medium text-emerald-600 hover:text-emerald-500" target="_blank" rel="noreferrer">
+                                                                                                        {publication.title}
+                                                                                                    </a>
+                                                                                                </span>
+                                                                                            </div>
+                                                                                        </li>
+                                                                                    ))
+                                                                                }
+                                                                            </ul>
+                                                                        }
+                                                                    </div>
+                                                                </section>
                                                             </li>
                                                         ))
                                                     }
@@ -439,10 +451,7 @@ const Profile = () => {
                                         </div>
                                     </div>
                                 </section>
-
                             }
-
-
                         </div>
                     </div>
                 </main>
