132a9cc748894e87119e8a470a434f9c116a80dc
Cloudy Young <yunfan.yang@agvisorpro.com>
2022-06-18 13:37:08 -0600

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,11 @@ import React from "react";
 
 import { classNames } from "./Utils";
 
-
 const Button = ({ onClick, loading, disabled, children, className, arrow }) => {
-
     return (
         <button
             type="button"
-            className={classNames("btn text-center w-full flex justify-center px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:opacity-50 disabled:cursor-not-allowed relative", className)}
+            className={classNames("btn text-center w-full flex justify-center px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:opacity-50 disabled:cursor-not-allowed relative", loading && "cursor-wait disabled:cursor-wait", className)}
             onClick={onClick}
             disabled={disabled}
         >
