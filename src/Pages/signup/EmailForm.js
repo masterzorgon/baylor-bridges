0b1e1c72494de2f62403a1bdfbb6d3511039e439
Tina <xiayan_li2@baylor.edu>
2022-06-23 22:07:52 +0800

    fix the styling for complete email form

--- src/Pages/signup/EmailForm.js
+++ src/Pages/signup/EmailForm.js
@@ -10,7 +10,7 @@ import axios from "axios";
 const EmailForm = () => {
     const [loading, setLoading] = useState(false);
     const [complete, setComplete] = useState(false);
-    const [step, setStep] = useState(1);
+    const [step, setStep] = useState(2);
     const [error_message, setErrorMessage] = useState(null);
 
     const [email, setEmail] = useState("");
@@ -104,12 +104,12 @@ const EmailForm = () => {
     const step2 = () => {
         return (
             <div className="md:mx-10 my-8">
-                <h2 className="mt-6 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                <h2 className="mt-6 font-serif text-xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
                     {/* <span className="text-gradient bg-gradient-to-r from-emerald-600 via-emerald-500 to-teal-500">Welcome!</span> */}
                     <span className="block">You are almost there!</span>
                 </h2>
-                <p className="mt-4 mx-auto text-gray-700">
-                    We send the <span className="underline underline-offset-4 decoration-emerald-400">verification link</span> to your email {email},
+                <p className="mt-5 mx-auto text-gray-700">
+                    We sent the <span className="underline underline-offset-4 decoration-emerald-400">verification link</span> to your email {email},
                     please check your inboxes and complete the sign-up process!
                     we are glad to have you joining our Baylor Bridges Family :)
 
