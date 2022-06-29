517147782cc16046f6234e58df1977f089b9f9cf
Nathan Galindo <nathan.zebedee@gmail.com>
2022-06-29 11:59:27 -0500

    added list component input fields

--- src/Pages/setup/Modal.js
+++ src/Pages/setup/Modal.js
@@ -6,6 +6,11 @@ import { states, classNames } from "../../components/Utils";
 
 import Buttons from "./modals/components/Buttons";
 
+const semesters = [
+    { title: "Spring", value: "spring" },
+    { title: "Fall", value: "fall" }
+];
+
 const Modal = ({
     show,
     transition,
@@ -19,6 +24,7 @@ const Modal = ({
 }) => {
 
     const [state, setState] = useState(states[0]);
+    const [semester, setSemester] = useState(semesters[0]);
 
     useEffect(() => {
         for (let state of states) {
@@ -31,6 +37,18 @@ const Modal = ({
     useEffect(() => {
         setAccount({ ...account, state: state.value });
     }, [state, setAccount]);
+
+    useEffect(() => {
+        for (let semester of semesters) {
+            if (semester.value === account.graduate_semester) {
+                setSemester(semester);
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        setAccount({ ...account, graduate_semester: semester.value });
+    }, [semester, setAccount]);
 
     const inputFieldLabel = (required, title) => (
         required
@@ -146,7 +164,7 @@ const Modal = ({
                                                 }
 
                                                 // RENDER LIST INPUT
-                                                if (field.type === "list") {
+                                                if (field.type === "list" && field.title === "State") {
                                                     return (
                                                         <>
                                                             <Listbox value={state} onChange={setState}>
@@ -154,7 +172,7 @@ const Modal = ({
                                                                     <>
                                                                         <div className="relative border border-gray-300 rounded-sm px-3 py-2 focus-within:z-10 focus-within:ring-1 focus-within:ring-emerald-600 focus-within:border-emerald-600 transition-colors">
                                                                             {inputFieldLabel(field.required, field.title)}
-                                                                            <Listbox.Button className="bg-white relative w-full rounded-md text-left cursor-default focus:outline-none focus:ring-0 sm:text-sm">
+                                                                            <Listbox.Button className="cursor-pointer bg-white relative w-full rounded-md text-left focus:outline-none focus:ring-0 sm:text-sm">
                                                                                 <span className="block truncate">{state.title}</span>
                                                                                 <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                                                                                     <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -204,6 +222,70 @@ const Modal = ({
                                                                     </>
                                                                 )}
                                                             </Listbox>
+                                                        </>
+                                                    );
+                                                }
+
+                                                if (field.type === "list" && field.title === "Semester") {
+                                                    return (
+                                                        <>
+                                                            <div className="relative border border-gray-300 rounded-md rounded-b-none px-3 py-2 focus-within:z-10 focus-within:ring-1 focus-within:ring-emerald-600 focus-within:border-emerald-600 transition-colors">
+                                                                {inputFieldLabel(field.required, field.title)}
+                                                                <Listbox value={semester} onChange={setSemester}>
+                                                                    {({ open }) => (
+                                                                        <>
+                                                                            <div className="p-0 mt-1 relative">
+                                                                                <Listbox.Button className="bg-white relative w-full rounded-md text-left py-1 cursor-default focus:outline-none focus:ring-0 sm:text-sm">
+                                                                                    <span className="block truncate">{semester.title}</span>
+                                                                                    <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                                                                                        <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                                                                    </span>
+                                                                                </Listbox.Button>
+
+                                                                                <Transition
+                                                                                    show={open}
+                                                                                    as={Fragment}
+                                                                                    leave="transition ease-in duration-100"
+                                                                                    leaveFrom="opacity-100"
+                                                                                    leaveTo="opacity-0"
+                                                                                >
+                                                                                    <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                                                                                        {semesters.map((semester) => (
+                                                                                            <Listbox.Option
+                                                                                                key={semester.value}
+                                                                                                className={({ active }) => classNames(
+                                                                                                    active ? "text-white bg-emerald-600" : "text-gray-900",
+                                                                                                    "cursor-default select-none relative py-2 pl-3 pr-9"
+                                                                                                )}
+                                                                                                value={semester}
+                                                                                            >
+                                                                                                {({ selected, active }) => (
+                                                                                                    <>
+                                                                                                        <span className={classNames(selected ? "font-semibold" : "font-normal", "block truncate")}>
+                                                                                                            {semester.title}
+                                                                                                        </span>
+
+                                                                                                        {selected ? (
+                                                                                                            <span
+                                                                                                                className={classNames(
+                                                                                                                    active ? "text-white" : "text-emerald-600",
+                                                                                                                    "absolute inset-y-0 right-0 flex items-center pr-4"
+                                                                                                                )}
+                                                                                                            >
+                                                                                                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                                                                            </span>
+                                                                                                        ) : null}
+                                                                                                    </>
+                                                                                                )}
+                                                                                            </Listbox.Option>
+                                                                                        ))}
+                                                                                    </Listbox.Options>
+                                                                                </Transition>
+                                                                            </div>
+                                                                        </>
+                                                                    )}
+                                                                </Listbox>
+                                                            </div>
                                                         </>
                                                     );
                                                 }
