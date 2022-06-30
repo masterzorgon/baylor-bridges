2b6c2da23f14ca8921ed054b65eaf78efe16bd5e
Nathan Galindo <nathan.zebedee@gmail.com>
2022-06-30 17:54:30 -0500

    added different list options

--- src/Pages/setup/components/ListInput.js
+++ src/Pages/setup/components/ListInput.js
@@ -59,35 +59,69 @@ const ListInput = ({ field, inputFieldLabel, setAccount, account }) => {
                                 leaveTo="opacity-0"
                             >
                                 <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-sm text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-                                    {semesters.map((listItem) => (
-                                        <Listbox.Option
-                                            key={listItem.value}
-                                            className={({ active }) => classNames(
-                                                active ? "text-white bg-emerald-600" : "text-gray-900",
-                                                "cursor-default select-none relative py-2 pl-3 pr-9"
-                                            )}
-                                            value={listItem}
-                                        >
-                                            {({ selected, active }) => (
-                                                <>
-                                                    <span className={classNames(selected ? "font-semibold" : "font-normal", "block truncate")}>
-                                                        {listItem.title}
-                                                    </span>
+                                    {
+                                        field.title === "State"
+                                            ?
+                                            states.map((listItem) => (
+                                                <Listbox.Option
+                                                    key={listItem.value}
+                                                    className={({ active }) => classNames(
+                                                        active ? "text-white bg-emerald-600" : "text-gray-900",
+                                                        "cursor-default select-none relative py-2 pl-3 pr-9"
+                                                    )}
+                                                    value={listItem}
+                                                >
+                                                    {({ selected, active }) => (
+                                                        <>
+                                                            <span className={classNames(selected ? "font-semibold" : "font-normal", "block truncate")}>
+                                                                {listItem.title}
+                                                            </span>
 
-                                                    {selected ? (
-                                                        <span
-                                                            className={classNames(
-                                                                active ? "text-white" : "text-emerald-600",
-                                                                "absolute inset-y-0 right-0 flex items-center pr-4"
-                                                            )}
-                                                        >
-                                                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                                        </span>
-                                                    ) : null}
-                                                </>
-                                            )}
-                                        </Listbox.Option>
-                                    ))}
+                                                            {selected ? (
+                                                                <span
+                                                                    className={classNames(
+                                                                        active ? "text-white" : "text-emerald-600",
+                                                                        "absolute inset-y-0 right-0 flex items-center pr-4"
+                                                                    )}
+                                                                >
+                                                                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                                </span>
+                                                            ) : null}
+                                                        </>
+                                                    )}
+                                                </Listbox.Option>
+                                            ))
+                                            :
+                                            semesters.map((listItem) => (
+                                                <Listbox.Option
+                                                    key={listItem.value}
+                                                    className={({ active }) => classNames(
+                                                        active ? "text-white bg-emerald-600" : "text-gray-900",
+                                                        "cursor-default select-none relative py-2 pl-3 pr-9"
+                                                    )}
+                                                    value={listItem}
+                                                >
+                                                    {({ selected, active }) => (
+                                                        <>
+                                                            <span className={classNames(selected ? "font-semibold" : "font-normal", "block truncate")}>
+                                                                {listItem.title}
+                                                            </span>
+
+                                                            {selected ? (
+                                                                <span
+                                                                    className={classNames(
+                                                                        active ? "text-white" : "text-emerald-600",
+                                                                        "absolute inset-y-0 right-0 flex items-center pr-4"
+                                                                    )}
+                                                                >
+                                                                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                                </span>
+                                                            ) : null}
+                                                        </>
+                                                    )}
+                                                </Listbox.Option>
+                                            ))
+                                    }
                                 </Listbox.Options>
                             </Transition>
                         </div>
