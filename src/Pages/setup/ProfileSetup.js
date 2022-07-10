19942699352920d7e3d2960fbcd9625137a3e356
Cloudy Young <yunfan.yang@agvisorpro.com>
2022-07-10 13:27:21 -0600

    Update modal dictionary

--- src/Pages/setup/ProfileSetup.js
+++ src/Pages/setup/ProfileSetup.js
@@ -7,13 +7,71 @@ import { useTransition } from "react-spring";
 import { CheckCircleIcon, UserCircleIcon, LocationMarkerIcon, AcademicCapIcon, BriefcaseIcon } from "@heroicons/react/outline";
 import { InboxIcon } from "@heroicons/react/outline";
 
-import { States } from "../../components/Utils";
-import { Semesters } from "../../components/profile/Fields";
+import { Properties } from "../../components/profile/Fields";
 
 import Modal from "./Modal";
 
-const InfoInput = () => {
 
+const fields = {
+    name: {
+        title: "Name",
+        sequence: 1,
+        description: "Please provide your full name. This is the name others will see on your profile for others who may reach you.",
+        icon: UserCircleIcon,
+        fields: [
+            Properties.name,
+        ]
+    },
+    contact: {
+        title: "Contact Information",
+        sequence: 2,
+        description: "Please provide the contact information through which you wish to be contacted. This information will be publicly displayed on your account.",
+        icon: InboxIcon,
+        fields: [
+            Properties.email,
+            Properties.phone,
+        ]
+    },
+    location: {
+        title: "Location Information",
+        sequence: 3,
+        description: "Please provide your location information. This information will be used to fill out our Baylor Bridges heat map as displayed on the home page.",
+        icon: LocationMarkerIcon,
+        fields: [
+            Properties.location,
+        ]
+    },
+    graduation: {
+        title: "Graduating Class",
+        sequence: 4,
+        description: "Please provide the year and semester of your graduating class from Baylor University. If you have not yet graduated, please provide the  graduation year and semester.",
+        icon: AcademicCapIcon,
+        fields: [
+            Properties.graduate_alumni,
+            Properties.graduate_student,
+        ]
+    },
+    headline: {
+        title: "Headline",
+        sequence: 5,
+        description: "Your headline should be your professional title, and your biography should be a summary of who you are and what you do.",
+        icon: BriefcaseIcon,
+        fields: [
+            Properties.headline,
+            Properties.occupation,
+            Properties.biography,
+        ]
+    },
+    done: {
+        title: "You're all set!",
+        subtitle: "Great job.",
+        sequence: 6,
+        description: "Thank you so much for taking the time to set up your Baylor Bridges account. We hope you enjoy our platform, and please feel to reach out via the Contact Us page if you have any questions or concerns.",
+        icon: CheckCircleIcon,
+    }
+};
+
+const InfoInput = () => {
     const [show, setShow] = useState(false); // used to fade modals in and out
     const [, , showTheModal] = useTimeoutFn(() => setShow(true), 400); // used to fade modals in
     const [, , takeAwayModal] = useTimeoutFn(() => setShow(false), 100); // used to fade modal out
@@ -39,9 +97,8 @@ const InfoInput = () => {
     // this makes the modal fade in on refresh
     useEffect(() => showTheModal(), [modal]);
 
+    // get current authenticated account profile
     const x_fields = "user_id, first_name, last_name, headline, role, occupation, graduate_year, graduate_semester, city, state, biography, contact_info";
-
-    // get current authenticated account
     useEffect(() => {
         axios.get("/accounts/me", { headers: { "x-fields": x_fields } })
             .then(res => {
@@ -49,9 +106,8 @@ const InfoInput = () => {
                 console.log(res.data);
             })
             .catch(err => {
-                err.response.status && err.response.status === 401
-                    ? window.location.href = "/sign-in"
-                    : window.location.href = "/404";
+                console.log(err);
+                toast.error(err.response.data.message);
             });
     }, []);
 
@@ -60,17 +116,16 @@ const InfoInput = () => {
         if (status === "next") {
             setLoading(true);
 
-            const accountCopy = { ...account };
-
-            try {
-                await axios.put("/accounts/me", accountCopy, { headers: { "x-fields": x_fields } });
-                setLoading(false);
-                setTimeout(() => setModal(modal + 1), 400);
-            } catch (error) {
-                toast.error(error.response.data.message);
-            }
-
-        } else setTimeout(() => setModal(modal - 1), 400);
+            axios.put("/accounts/me", account, { headers: { "x-fields": x_fields } })
+                .then(res => {
+                    setLoading(false);
+                    setTimeout(() => setModal(modal + 1), 400);
+                }).catch(err => {
+                    toast.error(err.response.data.message);
+                });
+        } else {
+            setTimeout(() => setModal(modal - 1), 400);
+        }
 
         takeAwayModal();
     };
@@ -86,194 +141,11 @@ const InfoInput = () => {
         console.log("ACCOUNT CHANGED:", account);
     }, [account]);
 
-    const modalInfo = {
-        NameInput: {
-            title: "Name",
-            sequence: 1,
-            description: "Please provide your full name. This is the name others will see on your profile for others who may reach you.",
-            icon: UserCircleIcon,
-            buttons: (account.first_name !== null && account.last_name !== null) && (account.first_name !== "" && account.last_name !== ""),
-            fields: [
-                {
-                    type: "text",
-                    title: "First Name (and Middle Name)",
-                    placeholder: "Peter",
-                    key: "first_name",
-                    required: true,
-                    value: account.first_name,
-                    filtered: false,
-                    change: event => setAccount({ ...account, first_name: event.target.value })
-                },
-                {
-                    type: "text",
-                    title: "Last Name",
-                    placeholder: "Parker",
-                    key: "last_name",
-                    required: true,
-                    value: account.last_name,
-                    filtered: false,
-                    change: event => setAccount({ ...account, last_name: event.target.value })
-                },
-            ]
-        },
-        ContactInput: {
-            title: "Contact Information",
-            sequence: 2,
-            description: "Please provide the contact information through which you wish to be contacted. This information will be publicly displayed on your account.",
-            icon: InboxIcon,
-            buttons: (account.contact_info.email !== null || account.contact_info.phone !== null) && (account.contact_info.email !== "" || account.contact_info.phone !== ""),
-            fields: [
-                {
-                    type: "text",
-                    title: "Phone Number",
-                    placeholder: "123-456-7890",
-                    key: "phone",
-                    required: false,
-                    value: account.contact_info.phone,
-                    filtered: false,
-                    change: event => handlePhoneInput(event)
-                },
-                {
-                    type: "text",
-                    title: "Email Address",
-                    placeholder: "example@email.com",
-                    key: "email",
-                    required: false,
-                    value: account.contact_info.email,
-                    filtered: false,
-                    change: event => setAccount({ ...account, contact_info: { ...account.contact_info, email: event.target.value } })
-                },
-            ]
-        },
-        LocationInput: {
-            title: "Location Information",
-            sequence: 3,
-            description: "Please provide your location information. This information will be used to fill out our Baylor Bridges heat map as displayed on the home page.",
-            icon: LocationMarkerIcon,
-            buttons: (account.state !== null && account.city !== null) && (account.state !== "" && account.city !== ""),
-            fields: [
-                {
-                    type: "list",
-                    title: "State",
-                    placeholder: "TX",
-                    key: "state",
-                    required: true,
-                    value: account.state,
-                    filtered: false,
-                    list: States
-                },
-                {
-                    type: "text",
-                    title: "City",
-                    placeholder: "San Antonio",
-                    key: "city",
-                    required: false,
-                    value: account.city,
-                    filtered: false,
-                    change: event => setAccount({ ...account, city: event.target.value })
-                }
-            ]
-        },
-        GradInput: {
-            title: "Graduating Class",
-            sequence: 4,
-            description: "Please provide the year and semester of your graduating class from Baylor University. If you have not yet graduated, please provide the  graduation year and semester.",
-            icon: AcademicCapIcon,
-            buttons: (account.graduate_year !== null && account.graduate_semester !== null) && (account.graduate_year !== "" && account.graduate_semester !== ""),
-            fields: [
-                {
-                    type: "list",
-                    title: "Semester",
-                    placeholder: "Spring",
-                    key: "graduate_semester",
-                    required: false,
-                    value: account.graduate_semester,
-                    filtered: false,
-                    list: Semesters
-                },
-                {
-                    type: "text",
-                    title: "Year",
-                    placeholder: "2001",
-                    key: "graduate_year",
-                    required: false,
-                    value: account.graduate_year,
-                    filtered: true,
-                    change: event => handleGradYearInput(event)
-                }
-            ]
-        },
-        HeadlineInput: {
-            title: "Headline",
-            sequence: 5,
-            description: "Your headline should be your professional title, and your biography should be a summary of who you are and what you do.",
-            icon: BriefcaseIcon,
-            buttons: (account.headline !== null && account.biography !== null) && (account.headline !== "" && account.biography !== ""),
-            fields: [
-                {
-                    type: "text",
-                    title: "Headline",
-                    placeholder: "Cardiovascular Surgeon",
-                    key: "headline",
-                    required: false,
-                    value: account.headline,
-                    filtered: false,
-                    change: event => setAccount({ ...account, headline: event.target.value })
-                },
-                {
-                    type: "bio",
-                    title: "Biography",
-                    placeholder: "I am a cardiovascular surgeon at St. Luke's, where I...",
-                    key: "biography",
-                    required: false,
-                    value: account.biography,
-                    filtered: false,
-                    change: event => setAccount({ ...account, biography: event.target.biography })
-                }
-            ]
-        },
-        AllDone: {
-            title: "You're all set!",
-            subtitle: "Great job.",
-            sequence: 6,
-            description: "Thank you so much for taking the time to set up your Baylor Bridges account. We hope you enjoy our platform, and please feel to reach out via the Contact Us page if you have any questions or concerns.",
-            icon: CheckCircleIcon,
-            buttons: true,
-        }
-    };
-
-    const handleGradYearInput = event => {
-        // verify that it is less than or equal to 4 characters
-        // verify that the value provided is a number
-        // take into consideration the user deletes their input and accidentally inputs a space char
-        if (event.target.value.length <= 4 && !isNaN(event.target.value)) {
-            setAccount({ ...account, graduate_year: parseInt(event.target.value) });
-        }
-        if (event.target.value === "" || event.target.value === " ") {
-            setAccount({ ...account, graduate_year: "" });
-        }
-    };
-
-    const handlePhoneInput = event => {
-        // only allow numbers, dashes, and spaces, and parentheses
-        let latestInput = event.target.value[event.target.value.length - 1];
-        if (
-            !isNaN(latestInput || event.target.value) ||
-            latestInput === " " ||
-            latestInput === "-" ||
-            latestInput === "(" ||
-            latestInput === ")"
-        ) {
-            setAccount({ ...account, contact_info: { ...account.contact_info, phone: event.target.value } });
-            console.log("PHONE", account.contact_info.phone);
-        }
-    };
-
     // RENDER MODAL COMPONENTS
-    for (const property in modalInfo) {
+    for (const property in fields) {
 
-        if (modalInfo[property].sequence === modal) {
-            const modalField = modalInfo[property];
+        if (fields[property].sequence === modal) {
+            const modalField = fields[property];
 
             return (
                 <>
